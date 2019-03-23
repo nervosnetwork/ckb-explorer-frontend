88ca3e1e2cfb9bbfc22b6bfef427a98b4c181dc3
duanyytop <duanyytop@gmail.com>
2019-03-23 20:59:05 +0800

    feat: add header of home page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -5,32 +5,72 @@ import Header from '../../components/Header'
 import Content from '../../components/Content'
 import Footer from '../../components/Footer'
 import HomeLogo from '../../asserts/logo_home.png'
+import SearchLogo from '../../asserts/search.png'
 
 const HomeHeader = styled.div`
-  height: 335px;
+  height: 670px;
   width: 100%;
-  background: rgb(24,50,93);
+  background: rgb(24, 50, 93);
+  display: flex;
+  flex-direction: column;
 `
 
 const LogoPanel = styled.div`
   position: relative;
-  top: 73.5px;
-  height: 74.5px;
+  top: 147px;
+  height: 149px;
   width: auto;
+  justify-content: center;
   dispaly: flex;
   display: -webkit-flex; /* Safari */
-  justify-content: center;
   flex-direction: row;
   img {
-    width: 78px;
-    height: 74.5px;
+    width: 156px;
+    height: 149px;
   }
   div {
-    line-height: 74.5px;
-    margin-left: 14.5px;
+    line-height: 156px;
+    margin-left: 29px;
     color: #46ab81;
-    font-size: 22px;
+    font-size: 40px;
     font-weight: bold;
+  }
+`
+
+const SearchPanel = styled.div`
+  align-items: center;
+  position: relative;
+  top: 247px;
+  width: auto;
+  height: 65px;
+  text-align: center;
+  input {
+    position: relative;
+    width: 650px;
+    color: #bababa;
+    height: 65px;
+    font-size: 16px;
+    padding-left: 20px;
+    padding-right: 106px;
+    opacity: 0.2;
+    border-radius: 6px;
+    background-color: #ffffff;
+    &: focus {
+      color: black;
+      opacity: 1;
+    }
+  }
+  img {
+    position: relative;
+    top: 14px;
+    right: 50px;
+    width: 41px;
+    height: 41px;
+    opacity: 0.8;
+    &: hover {
+      opacity: 1;
+      cursor: pointer;
+    }
   }
 `
 
@@ -41,9 +81,13 @@ export default () => {
       <Content>
         <HomeHeader>
           <LogoPanel>
-            <img src={HomeLogo} alt='home logo'></img>
+            <img src={HomeLogo} alt="home logo" />
             <div>CKB Explorer Testnet</div>
           </LogoPanel>
+          <SearchPanel>
+            <input placeholder="Block Heigth / Block Hash / TxHash / Address" />
+            <img src={SearchLogo} alt="search logo" />
+          </SearchPanel>
         </HomeHeader>
       </Content>
       <Footer />
