f02821534b552006a83a9ddafec2c79d544c33ca
duanyytop <duanyytop@gmail.com>
2019-03-25 15:38:12 +0800

    feat: add home table title

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -7,17 +7,23 @@ import Footer from '../../components/Footer'
 import HomeLogo from '../../asserts/logo_home.png'
 import SearchLogo from '../../asserts/search.png'
 
-const HomeHeader = styled.div`
-  height: 670px;
+const HomeHeaderPanel = styled.div`
+  height: ${(props: { width: number }) => (670 * props.width) / 1920}px;
   width: 100%;
   background: rgb(24, 50, 93);
   display: flex;
   flex-direction: column;
+  justify-content: center;
+`
+
+const HomeHeader = styled.div`
+  width: 100%;
+  display: flex;
+  display: -webkit-flex; /* Safari */
+  flex-direction: column;
 `
 
 const LogoPanel = styled.div`
-  position: relative;
-  top: 147px;
   height: 149px;
   width: auto;
   justify-content: center;
@@ -32,15 +38,13 @@ const LogoPanel = styled.div`
     line-height: 156px;
     margin-left: 29px;
     color: #46ab81;
-    font-size: 40px;
+    font-size: 50px;
     font-weight: bold;
   }
 `
 
 const SearchPanel = styled.div`
-  align-items: center;
-  position: relative;
-  top: 247px;
+  margin-top: ${(props: { width: number }) => (98 * props.width) / 1920}px;
   width: auto;
   height: 65px;
   text-align: center;
@@ -74,21 +78,55 @@ const SearchPanel = styled.div`
   }
 `
 
+const BlockPanel = styled.div`
+  width: 100%;
+  height: 200px;
+  margin-top: ${(props: { width: number }) => (150 * props.width) / 1920}px;
+  margin-bottom: ${(props: { width: number }) => (200 * props.width) / 1920}px;
+`
+
+const ContentTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    font-size: 50px;
+    color: black;
+    margin: 0 auto;
+  }
+
+  span {
+    background: #46ab81;
+    height: 4px;
+    width: 197px;
+    margin: 0 auto;
+  }
+`
+
 export default () => {
   return (
     <Page>
       <Header search={false} />
       <Content>
-        <HomeHeader>
-          <LogoPanel>
-            <img src={HomeLogo} alt="home logo" />
-            <div>CKB Explorer Testnet</div>
-          </LogoPanel>
-          <SearchPanel>
-            <input placeholder="Block Heigth / Block Hash / TxHash / Address" />
-            <img src={SearchLogo} alt="search logo" />
-          </SearchPanel>
-        </HomeHeader>
+        <HomeHeaderPanel width={window.innerWidth}>
+          <HomeHeader>
+            <LogoPanel>
+              <img src={HomeLogo} alt="home logo" />
+              <div>CKB Testnet Explorer</div>
+            </LogoPanel>
+            <SearchPanel width={window.innerWidth}>
+              <input placeholder="Block Heigth / Block Hash / TxHash / Address" />
+              <img src={SearchLogo} alt="search logo" />
+            </SearchPanel>
+          </HomeHeader>
+        </HomeHeaderPanel>
+
+        <BlockPanel width={window.innerWidth}>
+          <ContentTitle>
+            <div>Latest Blocks</div>
+            <span />
+          </ContentTitle>
+        </BlockPanel>
       </Content>
       <Footer />
     </Page>
