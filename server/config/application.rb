4d74922ac8a334d40c16afaca69168119804b191
shaojunda <shaojunda@gmail.com>
2019-03-27 16:19:26 +0800

    feat: add gems

--- server/config/application.rb
+++ server/config/application.rb
@@ -31,5 +31,12 @@ module Server
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :options]
+      end
+    end
   end
 end
