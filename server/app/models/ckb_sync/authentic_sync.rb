18d37566eb88f1e61c009833c76ea14858a8da6b
shaojunda <shaojunda@gmail.com>
2019-03-29 16:20:27 +0800

    feat(server): add sync control

---
+++ server/app/models/ckb_sync/authentic_sync.rb
@@ -0,0 +1,23 @@
+class CkbSync::AuthenticSync
+  class << self
+    def start
+      loop do
+        sync_node_data
+        sleep(10)
+      end
+    end
+
+    private
+
+    def sync_node_data
+      local_tip_block_number = SyncInfo.local_authentic_tip_block_number
+      node_tip_block_number = CkbSync::Api.get_tip_block_number
+
+      ((local_tip_block_number + 1)..(node_tip_block_number - 10)).each do |number|
+        block_hash = CkbSync::Api.get_block_hash(number)
+        CheckBlockWorker.perform_async(block_hash)
+        SyncInfo.local_authentic_tip_block_number = number
+      end
+    end
+  end
+end
--- server/app/models/sync_info.rb
+++ server/app/models/sync_info.rb
@@ -1,4 +1,33 @@
 class SyncInfo < ApplicationRecord
+  enum status: { syncing: 0, synced: 1 }
+
+  class << self
+    def local_inauthentic_tip_block_number
+      SyncInfo.find_by(name: "inauthentic_tip_block_number").try(:value) || -1
+    end
+
+    def local_authentic_tip_block_number
+      SyncInfo.find_by(name: "authentic_tip_block_number").try(:value) || -1
+    end
+
+    def local_inauthentic_tip_block_number=(block_number)
+      sync_info = SyncInfo.find_by(name: "inauthentic_tip_block_number")
+      if sync_info.nil?
+        SyncInfo.create(name: "inauthentic_tip_block_number", value: block_number)
+      else
+        sync_info.update(value: block_number, status: "syncing")
+      end
+    end
+
+    def local_authentic_tip_block_number=(block_number)
+      sync_info = SyncInfo.find_by(name: "inauthentic_tip_block_number")
+      if sync_info.nil?
+        SyncInfo.create(name: "authentic_tip_block_number", value: block_number)
+      else
+        sync_info.update(value: block_number, status: "syncing")
+      end
+    end
+  end
 end
 
 # == Schema Information
@@ -7,11 +36,13 @@ end
 #
 #  id         :bigint(8)        not null, primary key
 #  name       :string
-#  value      :integer
+#  value      :bigint(8)
+#  status     :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
 # Indexes
 #
-#  index_sync_infos_on_name  (name)
+#  index_sync_infos_on_name             (name) UNIQUE
+#  index_sync_infos_on_name_and_status  (name,status)
 #
