7e9600c518df5ad3c5f2c4fe07fdeec1b7a624e4
duanyytop <duanyytop@gmail.com>
2019-04-02 16:03:49 +0800

    feat: add block table

---
+++ web/src/pages/BlockList/index.css.tsx
@@ -0,0 +1,89 @@
+import styled from 'styled-components'
+
+export const BlockListPanel = styled.div`
+  width: 100%;
+  margin-top: ${(props: { width: number }) => (150 * props.width) / 1920}px;
+  margin-bottom: ${(props: { width: number }) => (200 * props.width) / 1920}px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const ContentTitle = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  div {
+    font-size: 50px;
+    color: black;
+    margin: 0 auto;
+  }
+
+  span {
+    background: #46ab81;
+    height: 4px;
+    width: 197px;
+    margin: 0 auto;
+  }
+`
+
+export const ContentTable = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 58px;
+`
+
+export const TableTitleRow = styled.tr`
+  background: rgb(75, 188, 142);
+  display: flex;
+  flex-direction: row;
+
+  th {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    width: 240px;
+    height: ${(props: { width: number }) => (78 * props.width) / 1920}px;
+  }
+
+  img {
+    width: 23px;
+    height: 23px;
+  }
+
+  div {
+    color: white;
+    font-size: 20px;
+    margin-left: 10px;
+  }
+`
+
+export const TableContentRow = styled.tr`
+  display: flex;
+
+  td {
+    width: 240px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`
+
+export const TableMinerContentPanel = styled.td`
+  height: 78px;
+
+  .table__miner__content {
+    max-width: 90px;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    color: ${(props: { color: string }) => props.color};
+    text-decoration: none;
+  }
+`
+
+export const BlocksPagition = styled.div`
+  margin-top: 80px;
+`
--- web/src/pages/BlockList/index.tsx
+++ web/src/pages/BlockList/index.tsx
@@ -1,19 +1,139 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import Pagination from 'rc-pagination'
+import 'rc-pagination/assets/index.css'
+import {
+  BlockListPanel,
+  ContentTitle,
+  ContentTable,
+  TableTitleRow,
+  TableContentRow,
+  TableMinerContentPanel,
+  BlocksPagition,
+} from './index.css'
 import Page from '../../components/Page'
 import Header from '../../components/Header'
 import Content from '../../components/Content'
 import Footer from '../../components/Footer'
+import BlockHeightIcon from '../../asserts/block_height.png'
+import TransactionIcon from '../../asserts/transactions.png'
+import CellConsumedIcon from '../../asserts/cell_consumed.png'
+import MinerIcon from '../../asserts/miner.png'
+import TimestampIcon from '../../asserts/timestamp.png'
+import BlocksData from './mock'
+
+const TableTitleItem = ({ image, title }: { image: string; title: string }) => {
+  return (
+    <th>
+      <img src={image} alt={title} />
+      <div>{title}</div>
+    </th>
+  )
+}
+
+const TableContentItem = ({ color, content }: { color: string; content: string }) => {
+  return (
+    <td
+      style={{
+        color,
+        height: (78 * window.innerWidth) / 1920,
+      }}
+    >
+      {content}
+    </td>
+  )
+}
+
+const TableMinerContentItem = ({ color, content }: { color: string; content: string }) => {
+  return (
+    <TableMinerContentPanel color={color}>
+      <Link className="table__miner__content" to={`/address/${content}`}>
+        {content}
+      </Link>
+    </TableMinerContentPanel>
+  )
+}
+
+const formatData = (data: number) => {
+  return data < 10 ? `0${data}` : data
+}
+
+const parseDate = (timestamp: number) => {
+  const now = new Date().getTime()
+  const diff = (now - timestamp) / 1000
+  if (diff < 60) {
+    return `${diff} secs ago`
+  }
+  if (diff < 3600) {
+    return `${diff / 60} minutes ${diff % 60} secs ago`
+  }
+  const date = new Date(timestamp)
+  return `${date.getFullYear()}/${date.getMonth() + 1}/${date.getDate()} ${formatData(date.getHours())}:${formatData(
+    date.getMinutes(),
+  )}:${formatData(date.getSeconds())}`
+}
 
 export default () => {
+  const [clickableColor, normalColor] = ['#4bbc8e', '888888']
+
+  const PageSize = 10
+  const [currentPageNo, setCurrentPageNo] = useState(1)
+
+  // TODO: fetch transaction data from server
+  const getBlocks = (pageNo: number, pageSize: number) => {
+    return BlocksData.data.slice((pageNo - 1) * pageSize, pageNo * pageSize)
+  }
+
+  const onChange = (current: number, pageSize: number) => {
+    setCurrentPageNo(current)
+    getBlocks(current, pageSize)
+  }
+
   return (
     <Page>
       <Header />
-      <Content
-        style={{
-          padding: 20,
-        }}
-      >
-        <div className="container">Block</div>
+      <Content>
+        <BlockListPanel width={window.innerWidth}>
+          <ContentTitle>
+            <div>Blocks</div>
+            <span />
+          </ContentTitle>
+
+          <ContentTable>
+            <table>
+              <tbody>
+                <TableTitleRow width={window.innerWidth}>
+                  <TableTitleItem image={BlockHeightIcon} title="Height" />
+                  <TableTitleItem image={TransactionIcon} title="Transactions" />
+                  <TableTitleItem image={CellConsumedIcon} title="Cell Consumed(B)" />
+                  <TableTitleItem image={MinerIcon} title="Miner" />
+                  <TableTitleItem image={TimestampIcon} title="Time" />
+                </TableTitleRow>
+                {getBlocks(currentPageNo, PageSize).map((data: any) => {
+                  return (
+                    <TableContentRow key={data.block_hash}>
+                      <TableContentItem color={clickableColor} content={data.number} />
+                      <TableContentItem color={normalColor} content={data.transactions_count} />
+                      <TableContentItem color={normalColor} content={data.cell_consumed} />
+                      <TableMinerContentItem color={clickableColor} content={data.miner_hash} />
+                      <TableContentItem color={normalColor} content={parseDate(data.timestamp)} />
+                    </TableContentRow>
+                  )
+                })}
+              </tbody>
+            </table>
+          </ContentTable>
+          <BlocksPagition>
+            <Pagination
+              showQuickJumper
+              showSizeChanger
+              defaultPageSize={PageSize}
+              defaultCurrent={1}
+              total={BlocksData.data.length}
+              onChange={onChange}
+            />
+          </BlocksPagition>
+        </BlockListPanel>
       </Content>
       <Footer />
     </Page>
