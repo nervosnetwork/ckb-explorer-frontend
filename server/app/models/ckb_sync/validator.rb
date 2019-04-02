9981d94c3e2a3d28af0bd56927b52e4b00144e30
shaojunda <shaojunda@gmail.com>
2019-04-02 10:39:57 +0800

    feat(server): update account balance and cell consumed

--- server/app/models/ckb_sync/validator.rb
+++ server/app/models/ckb_sync/validator.rb
@@ -3,7 +3,22 @@ class CkbSync::Validator
     def call(block_hash)
       node_block = CkbSync::Api.get_block(block_hash).deep_stringify_keys
       local_block = Block.find_by(number: node_block.dig("header", "number"))
-      local_block.verify!(block.dig("header", "hash"))
+      ApplicationRecord.transaction do
+        local_block.verify!(block.dig("header", "hash"))
+        update_account_balance_and_cell_consumed!(local_block)
+      end
+    end
+
+    private
+
+    def update_account_balance_and_cell_consumed!(local_block)
+      accounts = local_block.contained_accounts.map do |account|
+        account.balance = CKB::Utils.get_balance(account.address_hash)
+        account.cell_consumed = CKB::Utils.account_cell_consumed(account.address_hash)
+        account
+      end
+
+      Account.import! accounts, on_duplicate_key_update: [:balance]
     end
   end
 end
