3e429196bc947f0b90b70fa606124e1dee500a19
shaojunda <shaojunda@gmail.com>
2019-04-11 16:32:15 +0800

    chore(server): add more factories

--- server/test/factories/ckb_transaction.rb
+++ server/test/factories/ckb_transaction.rb
@@ -10,5 +10,29 @@ FactoryBot.define do
     transaction_fee { 0 }
     version { 0 }
     witnesses {}
+
+
+    transient do
+      account { nil }
+    end
+
+    transient do
+      binary_hash { nil }
+    end
+
+    transient do
+      args { nil }
+    end
+
+    transient do
+      lock_script_version { nil }
+    end
+
+    trait :with_cell_output_and_lock_script do
+      after(:create) do |ckb_transaction, evaluator|
+        cell_output = create(:cell_output, ckb_transaction: ckb_transaction)
+        create(:lock_script, cell_output: cell_output, account: evaluator.account, args: evaluator.args, binary_hash: evaluator.binary_hash, version: evaluator.lock_script_version)
+      end
+    end
   end
 end
---
+++ server/test/factories/lock_script.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :lock_script do
+    args {}
+    binary_hash {}
+  end
+end
