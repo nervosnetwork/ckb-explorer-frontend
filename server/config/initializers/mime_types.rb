fc5b6551e4728150790dc3ab0d2b50b4b92bb759
shaojunda <shaojunda@gmail.com>
2019-04-15 15:09:45 +0800

    chore(server): registe api mime types

--- server/config/initializers/mime_types.rb
+++ server/config/initializers/mime_types.rb
@@ -2,3 +2,10 @@
 
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
+
+api_mime_types = %W(
+  application/vnd.api+json
+  text/x-json
+  application/json
+)
+Mime::Type.register 'application/vnd.api+json', :json, api_mime_types
