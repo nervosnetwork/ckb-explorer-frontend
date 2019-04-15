e1da75be6e5d9586789a3b0e604e650b90d0c6b2
shaojunda <shaojunda@gmail.com>
2019-04-15 20:54:01 +0800

    feat(server): add api error handler

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -1,2 +1,25 @@
 class ApplicationController < ActionController::API
+  rescue_from Api::V1::Exceptions::Error, with: :api_error
+
+  private
+
+  def api_error(error)
+    render json: {
+      message: error.title,
+      errors: [
+        {
+          code: error.code,
+          status: error.status,
+          title: error.title,
+          detail: error.detail,
+          href: error.href
+        }
+      ]
+    }, status: error.status
+  end
+
+  def check_header_info
+    raise Api::V1::Exceptions::WrongContentTypeError if request.headers["Content-Type"] != "application/vnd.api+json"
+    raise Api::V1::Exceptions::WrongAcceptError if request.headers["Accept"] != "application/vnd.api+json"
+  end
 end
