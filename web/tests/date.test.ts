db7d6983272fdb3877b4570d4dc8e62dfb230115
duanyytop <duanyytop@gmail.com>
2019-04-16 19:33:39 +0800

    test: update date test cases

--- web/tests/date.test.ts
+++ web/tests/date.test.ts
@@ -1,14 +1,10 @@
-import { formatData, parseSimpleDate } from '../src/utils/date'
+import { formatData } from '../src/utils/date'
 
 describe('Date methods tests', () => {
 
   it('format date data', async () => {
-    expect("02").toBe(formatData(2))
-    expect(10).toBe(formatData(10))
-  })
-
-  it('format timestamp', async () => {
-    expect("2019/2/24 20:43:20").toBe(parseSimpleDate(1551012200000))
+    expect(formatData(2)).toBe("02")
+    expect(formatData(10)).toBe(10)
   })
 
 })
