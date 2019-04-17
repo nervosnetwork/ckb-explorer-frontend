91518ce0f0c628cb65c71319264c3e3d702bc134
shaojunda <shaojunda@gmail.com>
2019-04-17 16:19:52 +0800

    chore(server): Impl find records

--- server/app/controllers/api/v1/address_transactions_controller.rb
+++ server/app/controllers/api/v1/address_transactions_controller.rb
@@ -4,7 +4,10 @@ module Api
       before_action :validate_query_params
 
       def show
-        render json: {}
+        account = Account.find_by(address_hash: params[:id])
+        ckb_transactions = account.ckb_transactions
+
+        render json: CkbTransactionSerializer.new(ckb_transactions)
       end
 
       private
