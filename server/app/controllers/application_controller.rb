a7254d30b528a34804712170a372d1185569fe7f
shaojunda <shaojunda@gmail.com>
2019-04-17 14:58:01 +0800

    feat(server): add pagination validation

--- server/app/controllers/application_controller.rb
+++ server/app/controllers/application_controller.rb
@@ -4,12 +4,23 @@ class ApplicationController < ActionController::API
 
   private
 
-  def api_error(*error)
-    render json: RequestErrorSerializer.new(error, message: error.first.title), status: error.first.status
+  def api_error(error)
+    render json: RequestErrorSerializer.new([error], message: error.title), status: error.status
   end
 
   def check_header_info
     raise Api::V1::Exceptions::WrongContentTypeError if request.headers["Content-Type"] != "application/vnd.api+json"
     raise Api::V1::Exceptions::WrongAcceptError if request.headers["Accept"] != "application/vnd.api+json"
+  end
+
+  def validate_pagination_params
+    validator = Validations::Pagination.new(params)
+
+    if validator.invalid?
+      errors = validator.error_object[:errors]
+      status = validator.error_object[:status]
+
+      render json: errors, status: status
+    end
   end
 end
---
+++ server/app/controllers/validations/pagination.rb
@@ -0,0 +1,30 @@
+module Validations
+  class Pagination
+    include ActiveModel::Validations
+
+    validates :page, numericality: { only_integer: true }, allow_nil: true
+    validates :page_size, numericality: { only_integer: true }, allow_nil: true
+
+    def initialize(params={})
+      @page = params[:page]
+      @page_size = params[:page_size]
+    end
+
+    def error_object
+      api_errors = []
+
+      if invalid?
+        api_errors << Api::V1::Exceptions::PageParamError.new if :page.in?(errors.keys)
+        api_errors << Api::V1::Exceptions::PageSizeParamError.new if :page_size.in?(errors.keys)
+        {
+          status: api_errors.first.status,
+          errors: RequestErrorSerializer.new(api_errors, message: api_errors.first.title)
+        }
+      end
+    end
+
+    private
+
+    attr_accessor :page, :page_size
+  end
+end
