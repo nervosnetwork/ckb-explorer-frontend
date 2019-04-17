45f78fae02255100ab7c74e6ab5d7b3b2633d5a4
shaojunda <shaojunda@gmail.com>
2019-04-17 15:15:15 +0800

    chore(server): use tx validation

--- server/app/controllers/api/v1/ckb_transactions_controller.rb
+++ server/app/controllers/api/v1/ckb_transactions_controller.rb
@@ -1,10 +1,27 @@
 module Api
   module V1
     class CkbTransactionsController < ApplicationController
+      before_action :validate_query_params, only: :show
+
       def show
-        ckb_transaction = CkbTransaction.find_tx(params[:id])
+        ckb_transaction = CkbTransaction.find_by!(tx_hash: params[:id])
 
         render json: CkbTransactionSerializer.new(ckb_transaction)
+      rescue ActiveRecord::RecordNotFound
+        raise Api::V1::Exceptions::CkbTransactionNotFoundError
+      end
+
+      private
+
+      def validate_query_params
+        validator = Validations::CkbTransaction.new(params)
+
+        if validator.invalid?
+          errors = validator.error_object[:errors]
+          status = validator.error_object[:status]
+
+          render json: errors, status: status
+        end
       end
     end
   end
