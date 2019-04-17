7ec3d144af5aa31ee9d410a8c0b00535d580e5e0
shaojunda <shaojunda@gmail.com>
2019-04-17 17:29:39 +0800

    chore(server): add pagination

--- server/app/controllers/api/v1/address_transactions_controller.rb
+++ server/app/controllers/api/v1/address_transactions_controller.rb
@@ -2,11 +2,11 @@ module Api
   module V1
     class AddressTransactionsController < ApplicationController
       before_action :validate_query_params
-      before_action :validate_pagination_params
+      before_action :validate_pagination_params, :pagination_params
 
       def show
         account = Account.find_by!(address_hash: params[:id])
-        ckb_transactions = account.ckb_transactions
+        ckb_transactions = account.ckb_transactions.recent.page(@page).per(@page_size)
 
         render json: CkbTransactionSerializer.new(ckb_transactions)
       rescue ActiveRecord::RecordNotFound
@@ -24,6 +24,11 @@ module Api
 
           render json: errors, status: status
         end
+      end
+
+      def pagination_params
+        @page = params[:page] || 1
+        @page_size = params[:page_size] || CkbTransaction.default_per_page
       end
     end
   end
