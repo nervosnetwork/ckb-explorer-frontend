5416630f60c13dac8c29380aff56daaf55b89c0b
shaojunda <shaojunda@gmail.com>
2019-04-18 10:55:04 +0800

    feat(server): generate cell input lock scripts controller and test

---
+++ server/app/controllers/api/v1/cell_input_lock_scripts_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class CellInputLockScriptsController < ApplicationController
+    end
+  end
+end
--- server/config/routes.rb
+++ server/config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
       get "/transactions/:id", to: "ckb_transactions#show", as: "ckb_transaction"
       resources :address_transactions, only: :show
       resources :block_transactions, only: :show
+      resources :cell_input_lock_scripts, only: :show
     end
   end
 end
