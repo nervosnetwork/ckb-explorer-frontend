c2baf5e15376cd9a97d8c36fa5219c8573033d5a
shaojunda <shaojunda@gmail.com>
2019-04-18 09:43:47 +0800

    test(server): status code and header info

--- server/test/controllers/api/v1/block_transactions_controller_test.rb
+++ server/test/controllers/api/v1/block_transactions_controller_test.rb
@@ -3,7 +3,57 @@ require 'test_helper'
 module Api
   module V1
     class BlockTransactionsControllerTest < ActionDispatch::IntegrationTest
+      test "should get success code when call show" do
+        block = create(:block, :with_ckb_transactions)
 
+        valid_get api_v1_block_transaction_url(block.block_hash)
+
+        assert_response :success
+      end
+
+      test "should set right content type when call show" do
+        block = create(:block, :with_ckb_transactions)
+
+        valid_get api_v1_block_transaction_url(block.block_hash)
+
+        assert_equal "application/vnd.api+json", response.content_type
+      end
+
+      test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
+        block = create(:block, :with_ckb_transactions)
+
+        get api_v1_block_transaction_url(block.block_hash), headers: { "Content-Type": "text/plain" }
+
+        assert_equal 415, response.status
+      end
+
+      test "should respond with error object when Content-Type is wrong" do
+        block = create(:block, :with_ckb_transactions)
+        error_object = Api::V1::Exceptions::WrongContentTypeError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_block_transaction_url(block.block_hash), headers: { "Content-Type": "text/plain" }
+
+        assert_equal response_json, response.body
+      end
+
+      test "should respond with 406 Not Acceptable when Accept is wrong" do
+        block = create(:block, :with_ckb_transactions)
+
+        get api_v1_block_transaction_url(block.block_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal 406, response.status
+      end
+
+      test "should respond with error object when Accept is wrong" do
+        block = create(:block, :with_ckb_transactions)
+        error_object = Api::V1::Exceptions::WrongAcceptError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_block_transaction_url(block.block_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal response_json, response.body
+      end
     end
   end
 end
