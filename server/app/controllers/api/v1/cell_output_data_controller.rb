83b73e60f4ff5f7410faa909b4406c4b329317ab
shaojunda <shaojunda@gmail.com>
2019-04-18 20:35:35 +0800

    feat(server): generate controller and test
    for cell output data

---
+++ server/app/controllers/api/v1/cell_output_data_controller.rb
@@ -0,0 +1,6 @@
+module Api
+  module V1
+    class CellOutputDataController < ApplicationController
+    end
+  end
+end
---
+++ server/test/controllers/api/v1/cell_output_data_controller_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+
+module Api
+  module V1
+    class CellOutputDataControllerTest < ActionDispatch::IntegrationTest
+    end
+  end
+end
+
