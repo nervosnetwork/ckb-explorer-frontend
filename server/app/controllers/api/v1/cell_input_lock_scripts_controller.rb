0da625c48ac7b50bcfd9b2660d01991b1dfb5607
shaojunda <shaojunda@gmail.com>
2019-04-18 12:03:38 +0800

    feat(server): Impl find records
    when call /cell_input_lock_scripts/:id

--- server/app/controllers/api/v1/cell_input_lock_scripts_controller.rb
+++ server/app/controllers/api/v1/cell_input_lock_scripts_controller.rb
@@ -4,7 +4,10 @@ module Api
       before_action :validate_query_params
 
       def show
-        render json: {}
+        cell_input = CellInput.find(params[:id])
+        lock_script = cell_input.lock_script
+
+        render json: LockScriptSerializer.new(lock_script)
       end
 
       private
