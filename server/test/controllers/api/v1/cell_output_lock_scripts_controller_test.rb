f8f290b7ae7984a1a614de2e1865be1ca55b653a
shaojunda <shaojunda@gmail.com>
2019-04-18 19:47:10 +0800

    test(server): status code and header info
    when call /cell_output_lock_scripts/:id

--- server/test/controllers/api/v1/cell_output_lock_scripts_controller_test.rb
+++ server/test/controllers/api/v1/cell_output_lock_scripts_controller_test.rb
@@ -3,6 +3,57 @@ require "test_helper"
 module Api
   module V1
     class CellOutputLockScriptsControllerTest < ActionDispatch::IntegrationTest
+      test "should get success code when call show" do
+        cell_output = create(:cell_output, :with_full_transaction)
+
+        valid_get api_v1_cell_output_lock_script_url(cell_output.id)
+
+        assert_response :success
+      end
+
+      test "should set right content type when call show" do
+        cell_output = create(:cell_output, :with_full_transaction)
+
+        valid_get api_v1_cell_output_lock_script_url(cell_output.id)
+
+        assert_equal "application/vnd.api+json", response.content_type
+      end
+
+      test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
+        cell_output = create(:cell_output, :with_full_transaction)
+
+        get api_v1_cell_output_lock_script_url(cell_output.id), headers: { "Content-Type": "text/plain" }
+
+        assert_equal 415, response.status
+      end
+
+      test "should respond with error object when Content-Type is wrong" do
+        cell_output = create(:cell_output, :with_full_transaction)
+        error_object = Api::V1::Exceptions::WrongContentTypeError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_cell_output_lock_script_url(cell_output.id), headers: { "Content-Type": "text/plain" }
+
+        assert_equal response_json, response.body
+      end
+
+      test "should respond with 406 Not Acceptable when Accept is wrong" do
+        cell_output = create(:cell_output, :with_full_transaction)
+
+        get api_v1_cell_output_lock_script_url(cell_output.id), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal 406, response.status
+      end
+
+      test "should respond with error object when Accept is wrong" do
+        cell_output = create(:cell_output, :with_full_transaction)
+        error_object = Api::V1::Exceptions::WrongAcceptError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_cell_output_lock_script_url(cell_output.id), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal response_json, response.body
+      end
     end
   end
 end
--- server/test/factories/cell_output.rb
+++ server/test/factories/cell_output.rb
@@ -2,5 +2,12 @@ FactoryBot.define do
   factory :cell_output do
     capacity { 50000 }
     data {}
+
+    trait :with_full_transaction do
+      before(:create) do |cell_input, _evaluator|
+        ckb_transaction = create(:ckb_transaction, :with_cell_output_and_lock_script)
+        cell_input.update(ckb_transaction: ckb_transaction)
+      end
+    end
   end
 end
