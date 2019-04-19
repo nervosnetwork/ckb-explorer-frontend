de0055b943789cd51ed3a021417b0973d8939a68
shaojunda <shaojunda@gmail.com>
2019-04-19 11:29:05 +0800

    test(server): status code and header info
    when call /suggestqueries?q

--- server/test/controllers/api/v1/suggestqueries_controller_test.rb
+++ server/test/controllers/api/v1/suggestqueries_controller_test.rb
@@ -3,6 +3,47 @@ require "test_helper"
 module Api
   module V1
     class SuggestqueriesControllerTest < ActionDispatch::IntegrationTest
+      test "should get success code when call show" do
+        valid_get api_v1_suggestqueries_url(12)
+
+        assert_response :success
+      end
+
+      test "should set right content type when call show" do
+        valid_get api_v1_suggestqueries_url("0x3b238b3326d10ec000417b68bc715f17e86293d6cdbcb3fd8a628ad4a0b756f6")
+
+        assert_equal "application/vnd.api+json", response.content_type
+      end
+
+      test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
+        get api_v1_suggestqueries_url(12), headers: { "Content-Type": "text/plain" }
+
+        assert_equal 415, response.status
+      end
+
+      test "should respond with error object when Content-Type is wrong" do
+        error_object = Api::V1::Exceptions::WrongContentTypeError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_suggestqueries_url(12), headers: { "Content-Type": "text/plain" }
+
+        assert_equal response_json, response.body
+      end
+
+      test "should respond with 406 Not Acceptable when Accept is wrong" do
+        get api_v1_suggestqueries_url(12), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal 406, response.status
+      end
+
+      test "should respond with error object when Accept is wrong" do
+        error_object = Api::V1::Exceptions::WrongAcceptError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_suggestqueries_url(12), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal response_json, response.body
+      end
     end
   end
 end
