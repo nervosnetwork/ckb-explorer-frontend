ffe4d22f70896421dfed903bb35d7e06f919c9a9
shaojunda <shaojunda@gmail.com>
2019-04-19 09:40:25 +0800

    test(server): the correctness of the return values
    when call /addresses/:id

--- server/test/controllers/api/v1/addresses_controller_test.rb
+++ server/test/controllers/api/v1/addresses_controller_test.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class AddressesControllerTest < ActionDispatch::IntegrationTest
       test "should get success code when call show" do
-        address = create(:address)
+        address = create(:address, :with_lock_script)
 
         valid_get api_v1_address_url(address.address_hash)
 
@@ -12,7 +12,7 @@ module Api
       end
 
       test "should set right content type when call show" do
-        address = create(:address)
+        address = create(:address, :with_lock_script)
 
         valid_get api_v1_address_url(address.address_hash)
 
@@ -20,7 +20,7 @@ module Api
       end
 
       test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
-        address = create(:address)
+        address = create(:address, :with_lock_script)
 
         get api_v1_address_url(address.address_hash), headers: { "Content-Type": "text/plain" }
 
@@ -28,7 +28,7 @@ module Api
       end
 
       test "should respond with error object when Content-Type is wrong" do
-        address = create(:address)
+        address = create(:address, :with_lock_script)
         error_object = Api::V1::Exceptions::WrongContentTypeError.new
         response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
@@ -38,7 +38,7 @@ module Api
       end
 
       test "should respond with 406 Not Acceptable when Accept is wrong" do
-        address = create(:address)
+        address = create(:address, :with_lock_script)
 
         get api_v1_address_url(address.address_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
 
@@ -46,7 +46,7 @@ module Api
       end
 
       test "should respond with error object when Accept is wrong" do
-        address = create(:address)
+        address = create(:address, :with_lock_script)
         error_object = Api::V1::Exceptions::WrongAcceptError.new
         response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
@@ -72,6 +72,23 @@ module Api
 
         assert_equal response_json, response.body
       end
+
+      test "should return corresponding data with given address hash" do
+        address = create(:address, :with_lock_script)
+
+        valid_get api_v1_address_url(address.address_hash)
+
+        assert_equal AddressSerializer.new(address).serialized_json, response.body
+      end
+
+      test "should contain right keys in the serialized object when call show" do
+        address = create(:address, :with_lock_script)
+
+        valid_get api_v1_address_url(address.address_hash)
+
+        assert_equal %w(address_hash balance transactions_count cell_consumed lock_script).sort, json["data"]["attributes"].keys.sort
+      end
+
     end
   end
 end
