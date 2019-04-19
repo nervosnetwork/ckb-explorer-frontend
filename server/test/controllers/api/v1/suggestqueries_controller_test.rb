65c3f7674a9d797c37af9e7620f69c1a04dcd464
shaojunda <shaojunda@gmail.com>
2019-04-19 11:30:25 +0800

    test(server): invalid query param
    when call /suggestqueries?q

--- server/test/controllers/api/v1/suggestqueries_controller_test.rb
+++ server/test/controllers/api/v1/suggestqueries_controller_test.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class SuggestqueriesControllerTest < ActionDispatch::IntegrationTest
       test "should get success code when call show" do
-        valid_get api_v1_suggestqueries_url(12)
+        valid_get api_v1_suggestqueries_url, params: { q: "12" }
 
         assert_response :success
       end
@@ -16,7 +16,7 @@ module Api
       end
 
       test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
-        get api_v1_suggestqueries_url(12), headers: { "Content-Type": "text/plain" }
+        get api_v1_suggestqueries_url, params: { q: "12" }, headers: { "Content-Type": "text/plain" }
 
         assert_equal 415, response.status
       end
@@ -25,13 +25,13 @@ module Api
         error_object = Api::V1::Exceptions::WrongContentTypeError.new
         response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
-        get api_v1_suggestqueries_url(12), headers: { "Content-Type": "text/plain" }
+        get api_v1_suggestqueries_url, params: { q: "12" }, headers: { "Content-Type": "text/plain" }
 
         assert_equal response_json, response.body
       end
 
       test "should respond with 406 Not Acceptable when Accept is wrong" do
-        get api_v1_suggestqueries_url(12), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+        get api_v1_suggestqueries_url, params: { q: "12" }, headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
 
         assert_equal 406, response.status
       end
@@ -40,7 +40,34 @@ module Api
         error_object = Api::V1::Exceptions::WrongAcceptError.new
         response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
-        get api_v1_suggestqueries_url(12), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+        get api_v1_suggestqueries_url, params: { q: "12" }, headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal response_json, response.body
+      end
+
+      test "should response with error object when query key is neither integer nor hash" do
+        error_object = Api::V1::Exceptions::SuggestQueryKeyInvalidError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        valid_get api_v1_suggestqueries_url, params: { q: "0x3b238b3326d10ec000417b6&^&bc715f17e86293d6cdbcb3fd8a628ad4a0b756f6" }
+
+        assert_equal response_json, response.body
+      end
+
+      test "should response with error object when query key is not a hex start with 0x" do
+        error_object = Api::V1::Exceptions::SuggestQueryKeyInvalidError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        valid_get api_v1_suggestqueries_url, params: { q: "3b238b3326d10ec000417b68bc715f17e86293d6cdbcb3fd8a628ad4a0b756f6" }
+
+        assert_equal response_json, response.body
+      end
+
+      test "should return error object when query key is a hex start with 0x but the length is wrong" do
+        error_object = Api::V1::Exceptions::SuggestQueryKeyInvalidError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        valid_get api_v1_suggestqueries_url, params: { q: "0x3b238b3326d10ec0004" }
 
         assert_equal response_json, response.body
       end
