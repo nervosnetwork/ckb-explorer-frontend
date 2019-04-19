1c5a2f000eda047440d7d2e38dc51444c22b52ae
shaojunda <shaojunda@gmail.com>
2019-04-19 09:33:48 +0800

    test(server): status code and header info
    when call /addresses/:id

--- server/test/controllers/api/v1/addresses_controller_test.rb
+++ server/test/controllers/api/v1/addresses_controller_test.rb
@@ -3,6 +3,57 @@ require "test_helper"
 module Api
   module V1
     class AddressesControllerTest < ActionDispatch::IntegrationTest
+      test "should get success code when call show" do
+        address = create(:address)
+
+        valid_get api_v1_address_url(address.address_hash)
+
+        assert_response :success
+      end
+
+      test "should set right content type when call show" do
+        address = create(:address)
+
+        valid_get api_v1_address_url(address.address_hash)
+
+        assert_equal "application/vnd.api+json", response.content_type
+      end
+
+      test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
+        address = create(:address)
+
+        get api_v1_address_url(address.address_hash), headers: { "Content-Type": "text/plain" }
+
+        assert_equal 415, response.status
+      end
+
+      test "should respond with error object when Content-Type is wrong" do
+        address = create(:address)
+        error_object = Api::V1::Exceptions::WrongContentTypeError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_address_url(address.address_hash), headers: { "Content-Type": "text/plain" }
+
+        assert_equal response_json, response.body
+      end
+
+      test "should respond with 406 Not Acceptable when Accept is wrong" do
+        address = create(:address)
+
+        get api_v1_address_url(address.address_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal 406, response.status
+      end
+
+      test "should respond with error object when Accept is wrong" do
+        address = create(:address)
+        error_object = Api::V1::Exceptions::WrongAcceptError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+
+        get api_v1_address_url(address.address_hash), headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+
+        assert_equal response_json, response.body
+      end
     end
   end
 end
