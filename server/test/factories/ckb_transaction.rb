3bffe387b2a946a77b07fd931d7951d4528159f5
shaojunda <shaojunda@gmail.com>
2019-04-23 16:52:01 +0800

    test(server): other find function
    1. find_type_script!
    2. find_cell_output!

--- server/test/factories/ckb_transaction.rb
+++ server/test/factories/ckb_transaction.rb
@@ -30,7 +30,6 @@ FactoryBot.define do
 
     trait :with_cell_output_and_lock_script do
       after(:create) do |ckb_transaction, evaluator|
-        create(:cell_output, ckb_transaction: ckb_transaction)
         cell_outputs = create_list(:cell_output, 3, ckb_transaction: ckb_transaction)
         cell_outputs.map { |cell_output| create(:lock_script, cell_output: cell_output) }
       end
@@ -38,10 +37,11 @@ FactoryBot.define do
 
     trait :with_cell_output_and_lock_and_type_script do
       after(:create) do |ckb_transaction, evaluator|
-        cell_output = create(:cell_output, ckb_transaction: ckb_transaction)
-        create_list(:cell_output, 3, ckb_transaction: ckb_transaction)
-        create(:lock_script, cell_output: cell_output)
-        create(:type_script, cell_output: cell_output)
+        cell_outputs = create_list(:cell_output, 3, ckb_transaction: ckb_transaction)
+        cell_outputs.map do |cell_output|
+          create(:lock_script, cell_output: cell_output)
+          create(:type_script, cell_output: cell_output)
+        end
       end
     end
   end
--- server/test/models/cell_input_test.rb
+++ server/test/models/cell_input_test.rb
@@ -12,4 +12,20 @@ class CellInputTest < ActiveSupport::TestCase
 
     assert_equal previous_cell_output(cell_input.previous_output).lock_script, lock_script
   end
+
+  test "should return previous cell output's type script" do
+    cell_input = create(:cell_input, :with_full_transaction_and_type_script)
+
+    type_script = cell_input.find_type_script!
+
+    assert_equal previous_cell_output(cell_input.previous_output).type_script, type_script
+  end
+
+  test "should return previous cell output" do
+    cell_input = create(:cell_input, :with_full_transaction)
+
+    cell_output = cell_input.find_cell_output!
+
+    assert_equal previous_cell_output(cell_input.previous_output), cell_output
+  end
 end
