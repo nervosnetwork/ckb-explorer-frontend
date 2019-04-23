b212ab7849e0cd21f8ea961f3f51975a5ffb7ec5
duanyytop <duanyytop@gmail.com>
2019-04-23 14:11:43 +0800

    feat(ui): update search component for home page

--- web/src/pages/Home/index.tsx
+++ web/src/pages/Home/index.tsx
@@ -1,16 +1,14 @@
-import React, { useEffect, useState, useContext } from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
 import {
   HomeHeaderPanel,
   HomeHeader,
   LogoPanel,
-  SearchPanel,
   BlockPanel,
   ContentTitle,
   ContentTable,
   TableMorePanel,
 } from './styled'
-import AppContext from '../../contexts/App'
 import { parseDate } from '../../utils/date'
 import Content from '../../components/Content'
 import {
@@ -21,7 +19,7 @@ import {
   TableMinerContentItem,
 } from '../../components/Table'
 import HomeLogo from '../../asserts/logo_home.png'
-import SearchLogo from '../../asserts/search.png'
+import Search from '../../components/Search'
 import BlockHeightIcon from '../../asserts/block_height.png'
 import TransactionIcon from '../../asserts/transactions.png'
 import CellConsumedIcon from '../../asserts/cell_consumed.png'
@@ -29,15 +27,11 @@ import MinerIcon from '../../asserts/miner.png'
 import TimestampIcon from '../../asserts/timestamp.png'
 import MoreLeftIcon from '../../asserts/more_left.png'
 import MoreRightIcon from '../../asserts/more_right.png'
-import browserHistory from '../../routes/history'
-import { fetchBlocks, fetchSearchResult } from '../../http/fetcher'
+import { fetchBlocks } from '../../http/fetcher'
 import { BlockWrapper } from '../../http/response/Block'
 import { Response } from '../../http/response/Response'
-import { TransactionWrapper } from '../../http/response/Transaction'
-import { AddressWrapper } from '../../http/response/Address'
 
 export default () => {
-  const appContext = useContext(AppContext)
   const initBlockWrappers: BlockWrapper[] = []
   const [blocksWrappers, setBlocksWrappers] = useState(initBlockWrappers)
   useEffect(() => {
@@ -47,30 +41,6 @@ export default () => {
     })
   }, [])
 
-  const handleSearchResult = (q: string) => {
-    if (!q) {
-      appContext.toastMessage('Please input valid content', 3000)
-    } else {
-      fetchSearchResult(q)
-        .then((json: any) => {
-          const { data } = json
-          if (data.type === 'block') {
-            browserHistory.push(`/block/${(data as BlockWrapper).attributes.block_hash}`)
-          } else if (data.type === 'ckb_transaction') {
-            // interface here should change by backyard ckb_transaction to transaction
-            browserHistory.push(`/transaction/${(data as TransactionWrapper).attributes.transaction_hash}`)
-          } else if (data.type === 'address') {
-            browserHistory.push(`/address/${(data as AddressWrapper).attributes.address_hash}`)
-          } else {
-            browserHistory.push('/search/fail')
-          }
-        })
-        .catch(() => {
-          browserHistory.push(`/search/fail?q=${q}`)
-        })
-    }
-  }
-
   return (
     <Content>
       <HomeHeaderPanel width={window.innerWidth}>
@@ -79,28 +49,7 @@ export default () => {
             <img src={HomeLogo} alt="home logo" />
             <div>CKB Testnet Explorer</div>
           </LogoPanel>
-          <SearchPanel width={window.innerWidth}>
-            <input
-              id="home__search__bar"
-              placeholder="Block Heigth / Block Hash / TxHash / Address"
-              onKeyUp={(event: any) => {
-                if (event.keyCode === 13) {
-                  handleSearchResult(event.target.value)
-                }
-              }}
-            />
-            <div
-              role="button"
-              tabIndex={-1}
-              onKeyPress={() => {}}
-              onClick={() => {
-                const homeSearchBar = document.getElementById('home__search__bar') as HTMLInputElement
-                handleSearchResult(homeSearchBar.value)
-              }}
-            >
-              <img src={SearchLogo} alt="search logo" />
-            </div>
-          </SearchPanel>
+          <Search />
         </HomeHeader>
       </HomeHeaderPanel>
 
