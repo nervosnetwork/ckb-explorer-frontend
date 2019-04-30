e9bb59465bbe2c5711de61fb0bc44448a9528294
shaojunda <shaojunda@gmail.com>
2019-04-30 14:10:47 +0800

    test(server): authentic sync conflict

--- server/test/models/ckb_sync/authentic_sync_test.rb
+++ server/test/models/ckb_sync/authentic_sync_test.rb
@@ -6,14 +6,62 @@ module CkbSync
       SyncInfo.local_authentic_tip_block_number
     end
 
-    test "should create 11 blocks when inauthentic sync start" do
+    test "should create 11 blocks when inauthentic sync start and authentic sync failed" do
       Sidekiq::Testing.inline! do
-        CkbSync::Api.any_instance.stubs(:get_tip_block_number).returns(20)
+        SyncInfo.stubs(:local_inauthentic_tip_block_number).returns(20)
 
         assert_difference "Block.count", 11 do
           VCR.use_cassette("genesis_block") do
             VCR.use_cassette("blocks/two") do
               CkbSync::AuthenticSync.sync_node_data
+            end
+          end
+        end
+      end
+    end
+
+    test "should return nil when round range params to is negative" do
+      VCR.use_cassette("genesis_block") do
+        VCR.use_cassette("blocks/two") do
+          assert_nil CkbSync::AuthenticSync.sync_node_data
+        end
+      end
+    end
+
+    test "should return nil when latest round range is executing" do
+      VCR.use_cassette("genesis_block") do
+        VCR.use_cassette("blocks/four") do
+          CkbSync::Api.any_instance.stubs(:get_tip_block_number).returns(20)
+          CkbSync::InauthenticSync.sync_node_data
+        end
+      end
+
+      Rails.cache.stubs(:delete).returns(true)
+
+      VCR.use_cassette("genesis_block") do
+        VCR.use_cassette("blocks/two") do
+          CkbSync::AuthenticSync.sync_node_data
+        end
+      end
+      SyncInfo.stubs(:local_authentic_tip_block_number).returns(-1)
+      assert_nil CkbSync::AuthenticSync.sync_node_data
+    end
+
+    test "should verify 11 blocks when inauthentic sync start and authentic sync succeeded" do
+      Sidekiq::Testing.inline! do
+        VCR.use_cassette("genesis_block") do
+          VCR.use_cassette("blocks/four") do
+            CkbSync::Api.any_instance.stubs(:get_tip_block_number).returns(20)
+            CkbSync::InauthenticSync.sync_node_data
+          end
+        end
+
+        assert_difference "Block.count", 0 do
+          VCR.use_cassette("genesis_block") do
+            VCR.use_cassette("blocks/two") do
+              assert_changes -> {Block.where(status: "authentic").count}, from: 0, to: 11 do
+                CkbSync::AuthenticSync.sync_node_data
+              end
             end
           end
         end
