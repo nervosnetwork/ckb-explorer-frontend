0b0e4d179862a48e684087233eaef60e9daa6450
shaojunda <shaojunda@gmail.com>
2019-04-30 10:12:21 +0800

    chore(server): remove useless code

--- server/lib/ckb_authentic_sync.rb
+++ server/lib/ckb_authentic_sync.rb
@@ -3,7 +3,5 @@ require_relative "../config/environment"
 loop do
   CkbSync::AuthenticSync.sync_node_data
 
-  break if Rails.env.test?
-
   sleep(ENV["AUTHENTICSYNC_LOOP_INTERVAL"].to_i)
 end
--- server/lib/ckb_inauthentic_sync.rb
+++ server/lib/ckb_inauthentic_sync.rb
@@ -3,7 +3,5 @@ require_relative "../config/environment"
 loop do
   CkbSync::InauthenticSync.sync_node_data
 
-  break if Rails.env.test?
-
   sleep(ENV["INAUTHENTICSYNC_LOOP_INTERVAL"].to_i)
 end
