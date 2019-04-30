0a62240740862745f4666c7eef53ac6d24f446bd
shaojunda <shaojunda@gmail.com>
2019-04-30 14:13:35 +0800

    test(server): inauthentic sync conflict

--- server/test/models/ckb_sync/inauthentic_sync_test.rb
+++ server/test/models/ckb_sync/inauthentic_sync_test.rb
@@ -19,5 +19,23 @@ module CkbSync
         end
       end
     end
+
+    test "should return nil when latest round range is executing" do
+      CkbSync::Api.any_instance.stubs(:get_tip_block_number).returns(10)
+      Rails.cache.stubs(:delete).returns(true)
+
+      VCR.use_cassette("genesis_block") do
+        VCR.use_cassette("blocks/two") do
+          CkbSync::InauthenticSync.sync_node_data
+        end
+      end
+
+      VCR.use_cassette("genesis_block") do
+        VCR.use_cassette("blocks/two") do
+          SyncInfo.stubs(:local_inauthentic_tip_block_number).returns(-1)
+          assert_nil CkbSync::InauthenticSync.sync_node_data
+        end
+      end
+    end
   end
 end
