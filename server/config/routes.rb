f09cff6765b88c52ca2ea16bb49044db5c8a5d18
shaojunda <shaojunda@gmail.com>
2019-05-06 11:13:01 +0800

    feat(server): add sidekiq unique jobs gem

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   require "sidekiq/web"
+  require "sidekiq_unique_jobs/web"
   mount Sidekiq::Web => "/sidekiq"
 
   namespace :api do
