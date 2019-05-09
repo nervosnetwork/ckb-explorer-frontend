4a334ca8f025dd19de17fdcaf6794bd7a2d545c5
shaojunda <shaojunda@gmail.com>
2019-05-09 10:49:57 +0800

    chore(server): format code

--- server/config/initializers/config.rb
+++ server/config/initializers/config.rb
@@ -1,6 +1,6 @@
 Config.setup do |config|
   # Name of the constant exposing loaded settings
-  config.const_name = 'Settings'
+  config.const_name = "Settings"
 
   # Ability to remove elements of the array set in earlier loaded settings file. For example value: '--'.
   #
@@ -46,5 +46,4 @@ Config.setup do |config|
   #   required(:age).maybe(:int?)
   #   required(:email).filled(format?: EMAIL_REGEX)
   # end
-
 end
