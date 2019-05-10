9b76570acdab7e567d086f1b498ae4d9a18291c6
shaojunda <shaojunda@gmail.com>
2019-05-10 16:16:28 +0800

    chore(server): mov SYSTEM_SCRIPT_CELL_HASH to env file

--- server/app/models/lock_script.rb
+++ server/app/models/lock_script.rb
@@ -1,6 +1,4 @@
 class LockScript < ApplicationRecord
-  SYSTEM_SCRIPT_CELL_HASH = "0x28e83a1277d48add8e72fadaa9248559e1b632bab2bd60b27955ebc4c03800a5".freeze
-
   belongs_to :cell_output
   belongs_to :address
 
