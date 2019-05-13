51ff739b5b606a6f07e96fa4b67ee4c95a474e60
shaojunda <shaojunda@gmail.com>
2019-05-13 14:38:43 +0800

    chore(server): just return available records

--- server/app/controllers/api/v1/ckb_transactions_controller.rb
+++ server/app/controllers/api/v1/ckb_transactions_controller.rb
@@ -4,7 +4,7 @@ module Api
       before_action :validate_query_params, only: :show
 
       def show
-        ckb_transaction = CkbTransaction.find_by!(tx_hash: params[:id])
+        ckb_transaction = CkbTransaction.where(tx_hash: params[:id]).available.take!
 
         render json: CkbTransactionSerializer.new(ckb_transaction)
       rescue ActiveRecord::RecordNotFound
