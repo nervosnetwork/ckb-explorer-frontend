66e402dbf1ecbb787a3c0a1400f9e018bf14ff67
shaojunda <shaojunda@gmail.com>
2019-05-15 20:06:14 +0800

    fix: test

--- server/app/models/ckb_sync/api.rb
+++ server/app/models/ckb_sync/api.rb
@@ -2,7 +2,7 @@ module CkbSync
   class Api
     include Singleton
 
-    METHOD_NAMES = %w(rpc system_script_out_point dry_run_transaction calculate_dao_maximum_withdraw set_system_script_cell system_script_cell inspect system_script_cell_hash genesis_block get_block_by_number genesis_block_hash get_block_hash get_block get_tip_header get_tip_block_number get_cells_by_lock_hash get_transaction get_live_cell send_transaction local_node_info get_current_epoch get_epoch_by_number get_peers tx_pool_info get_blockchain_info get_peers_state compute_transaction_hash).freeze
+    METHOD_NAMES = %w(system_script_out_point dry_run_transaction calculate_dao_maximum_withdraw set_system_script_cell system_script_cell inspect system_script_cell_hash genesis_block get_block_by_number genesis_block_hash get_block_hash get_block get_tip_header get_tip_block_number get_cells_by_lock_hash get_transaction get_live_cell send_transaction local_node_info get_current_epoch get_epoch_by_number get_peers tx_pool_info get_blockchain_info get_peers_state compute_transaction_hash).freeze
 
     def initialize
       @hosts = Settings.hosts
--- server/test/models/ckb_sync/api_test.rb
+++ server/test/models/ckb_sync/api_test.rb
@@ -4,7 +4,7 @@ module CkbSync
   class ApiTest < ActiveSupport::TestCase
     test "should contain related methods" do
       contained_method_names = CkbSync::Api::METHOD_NAMES
-      sdk_api_names = CKB::API.instance_methods(false)
+      sdk_api_names = CKB::API.instance_methods(false).reject { |method| method == :rpc }
 
       assert_equal sdk_api_names.map(&:to_s).sort, contained_method_names.sort
     end
