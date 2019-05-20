fdb83e2cf275b2530e05d25054dcb0ad7dc9f4f7
shaojunda <shaojunda@gmail.com>
2019-05-20 18:52:01 +0800

    chore(server): use uncache

--- server/app/workers/validate_block_worker.rb
+++ server/app/workers/validate_block_worker.rb
@@ -3,6 +3,8 @@ class ValidateBlockWorker
   sidekiq_options queue: "authentic_sync", lock: :until_executed
 
   def perform(block_hash)
-    CkbSync::Validator.validate(block_hash)
+    ActiveRecord::Base.uncached do
+      CkbSync::Validator.validate(block_hash)
+    end
   end
 end
