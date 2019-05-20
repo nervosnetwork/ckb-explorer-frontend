1b0adea0ebad6047d0dcac13af9323b9d1e30371
shaojunda <shaojunda@gmail.com>
2019-05-20 15:28:56 +0800

    chore(server): move cors to initializer

--- server/config/application.rb
+++ server/config/application.rb
@@ -31,12 +31,5 @@ module Server
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins "*"
-        resource "*", headers: :any, methods: [:get, :options]
-      end
-    end
   end
 end
--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -14,3 +14,10 @@
 #       methods: [:get, :post, :put, :patch, :delete, :options, :head]
 #   end
 # end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins "*"
+    resource "*", headers: :any, methods: [:get, :options]
+  end
+end
