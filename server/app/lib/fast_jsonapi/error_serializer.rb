f2638c77ee5d065e2dc210d7515f3e72ca50f7fa
shaojunda <shaojunda@gmail.com>
2019-05-20 17:26:56 +0800

    chore(server): code format

--- server/app/lib/fast_jsonapi/error_serializer.rb
+++ server/app/lib/fast_jsonapi/error_serializer.rb
@@ -1,34 +1,30 @@
-require 'fast_jsonapi'
+require "fast_jsonapi"
 
-module FastJsonapi::ErrorSerializer
-  extend ActiveSupport::Concern
+module FastJsonapi
+  module ErrorSerializer
+    extend ActiveSupport::Concern
 
-  included do
-    attr_accessor :with_root_key
+    included do
+      attr_accessor :with_root_key
 
-    include FastJsonapi::ObjectSerializer
-    set_id :title
+      include FastJsonapi::ObjectSerializer
+      set_id :title
 
-    def initialize(resource, options = {})
-      super
-      @with_root_key = options[:with_root_key]
-      @message = options[:message]
-    end
+      def initialize(resource, options = {})
+        super
+        @with_root_key = options[:with_root_key]
+        @message = options[:message]
+      end
 
-    def hash_for_one_record
-      serialized_hash = super[:data][:attributes]
-      return !with_root_key ? serialized_hash : {
-        message: @message,
-        errors: serialized_hash
-      }
-    end
+      def hash_for_one_record
+        serialized_hash = super[:data][:attributes]
+        return !with_root_key ? serialized_hash : { message: @message,  errors: serialized_hash }
+      end
 
-    def hash_for_collection
-      serialized_hash = super[:data]&.map{|err| err[:attributes]}
-      return !with_root_key ? serialized_hash : {
-        message: @message,
-        errors: serialized_hash
-      }
+      def hash_for_collection
+        serialized_hash = super[:data]&.map { |err| err[:attributes] }
+        return !with_root_key ? serialized_hash : { message: @message, errors: serialized_hash }
+      end
     end
   end
 end
