93a564c09b99f28313de85fd81b6b8873bb3a2ad
shaojunda <shaojunda@gmail.com>
2019-05-23 09:56:22 +0800

    chore(server): no longer calculate address consumed

--- server/app/models/ckb_sync/validator.rb
+++ server/app/models/ckb_sync/validator.rb
@@ -23,7 +23,7 @@ module CkbSync
 
           local_block.verify!(node_block)
           update_cell_status!(local_block)
-          update_address_balance_and_cell_consumed!(local_block)
+          update_address_balance!(local_block)
         end
       end
 
@@ -43,21 +43,15 @@ module CkbSync
         CellOutput.where(id: cell_output_ids.delete(nil)).update_all(status: :dead)
       end
 
-      def update_address_balance_and_cell_consumed!(local_block)
+      def update_address_balance!(local_block)
         addresses = []
-        address_hashes = []
         local_block.contained_addresses.each do |address|
           address.balance = CkbUtils.get_balance(address.address_hash) || 0
-
-          if address.changed?
-            addresses << address
-            address_hashes << address.address_hash
-          end
+          addresses << address if address.changed?
         end
 
-        if !addresses.empty?
-          Sidekiq::Client.push_bulk("class" => "UpdateAddressCellConsumedWorker", "args" => address_hashes.map { |hash| [hash] }, "queue" => "address_cell_consumed_updater")
-          Address.import! addresses, on_duplicate_key_update: [:balance, :cell_consumed]
+        if addresses.present?
+          Address.import! addresses, on_duplicate_key_update: [:balance]
         end
       end
     end
