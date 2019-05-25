a8d5ab8b6812e816a1816fe8dea09380f66d8583
shaojunda <shaojunda@gmail.com>
2019-05-25 12:57:59 +0800

    chore(server): add lock expiration

--- server/app/workers/sync_block_worker.rb
+++ server/app/workers/sync_block_worker.rb
@@ -1,6 +1,6 @@
 class SyncBlockWorker
   include Sidekiq::Worker
-  sidekiq_options queue: "inauthentic_sync", lock: :until_executed
+  sidekiq_options queue: "inauthentic_sync", lock: :until_executed, lock_expiration: 1.minute
 
   def perform(block_number)
     CkbSync::Persist.sync(block_number)
--- server/app/workers/update_address_info_worker.rb
+++ server/app/workers/update_address_info_worker.rb
@@ -1,6 +1,6 @@
 class UpdateAddressInfoWorker
   include Sidekiq::Worker
-  sidekiq_options queue: "address_info_updater", lock: :until_executed
+  sidekiq_options queue: "address_info_updater", lock: :until_executed, lock_expiration: 1.minute
 
   def perform(address_id)
     address = Address.find(address_id)
