e2463b4867de061a8f25e0034d213a0491331269
shaojunda <shaojunda@gmail.com>
2019-05-27 16:41:50 +0800

    chore(server): use retry and remove lock expiration

--- server/app/workers/update_block_info_worker.rb
+++ server/app/workers/update_block_info_worker.rb
@@ -1,6 +1,6 @@
 class UpdateBlockInfoWorker
   include Sidekiq::Worker
-  sidekiq_options queue: "block_info_updater", lock: :until_executed, lock_expiration: 1.minute
+  sidekiq_options queue: "block_info_updater", lock: :until_executed
 
   def perform(transaction_id)
     ckb_transaction = CkbTransaction.find(transaction_id)
--- server/app/workers/validate_block_worker.rb
+++ server/app/workers/validate_block_worker.rb
@@ -1,6 +1,6 @@
 class ValidateBlockWorker
   include Sidekiq::Worker
-  sidekiq_options queue: "authentic_sync", lock: :until_executed, lock_expiration: 1.minute
+  sidekiq_options queue: "authentic_sync", lock: :until_executed
 
   def perform(block_hash)
     CkbSync::Validator.validate(block_hash)
