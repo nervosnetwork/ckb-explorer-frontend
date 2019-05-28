d292bb3384dc2d6e525953c3e53128a699759207
shaojunda <shaojunda@gmail.com>
2019-05-28 12:35:22 +0800

    chore(server): get current epoch number from database

--- server/db/migrate/20190528034230_update_block_reward.rb
+++ server/db/migrate/20190528034230_update_block_reward.rb
@@ -1,6 +1,6 @@
 class UpdateBlockReward < ActiveRecord::Migration[5.2]
   def up
-    current_epoch_number = api.get_current_epoch.number.to_i
+    current_epoch_number = Block.maximum(:epoch).to_i
     (0..current_epoch_number).each do |epoch|
       Block.where(epoch: epoch).update_all(reward: CkbUtils.miner_reward(epoch))
     end
