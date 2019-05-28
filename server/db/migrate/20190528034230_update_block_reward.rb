fc5d7572339402b1c94dea7e99fe8960ac6ec4d5
shaojunda <shaojunda@gmail.com>
2019-05-28 12:53:48 +0800

    fix(server): test

--- server/db/migrate/20190528034230_update_block_reward.rb
+++ server/db/migrate/20190528034230_update_block_reward.rb
@@ -1,5 +1,7 @@
 class UpdateBlockReward < ActiveRecord::Migration[5.2]
   def up
+    return if Rails.env.test?
+
     current_epoch_number = Block.maximum(:epoch).to_i
     (0..current_epoch_number).each do |epoch|
       Block.where(epoch: epoch).update_all(reward: CkbUtils.miner_reward(epoch))
