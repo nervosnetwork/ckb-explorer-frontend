6ceb38800800df5dbae3087c6dc9350c41803063
shaojunda <shaojunda@gmail.com>
2019-05-31 11:58:01 +0800

    chore(server): add transactions_root and witnesses root to block

--- server/app/serializers/block_serializer.rb
+++ server/app/serializers/block_serializer.rb
@@ -1,7 +1,7 @@
 class BlockSerializer
   include FastJsonapi::ObjectSerializer
 
-  attributes :block_hash, :number, :proposals_count, :uncles_count, :uncle_block_hashes, :reward, :total_transaction_fee, :cell_consumed, :total_cell_capacity, :miner_hash, :timestamp, :difficulty, :version, :epoch, :start_number, :length
+  attributes :block_hash, :number, :proposals_count, :uncles_count, :uncle_block_hashes, :reward, :total_transaction_fee, :cell_consumed, :total_cell_capacity, :miner_hash, :timestamp, :difficulty, :version, :epoch, :start_number, :length, :transactions_root, :witnesses_root
 
   attribute :nonce do |object|
     object.seal["nonce"]
