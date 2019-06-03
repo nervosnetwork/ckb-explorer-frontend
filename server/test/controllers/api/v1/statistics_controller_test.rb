ab142a331db329b3751cdded55352e32ce3fd436
shaojunda <shaojunda@gmail.com>
2019-06-03 14:14:18 +0800

    test(server): average_difficulty function

--- server/test/controllers/api/v1/statistics_controller_test.rb
+++ server/test/controllers/api/v1/statistics_controller_test.rb
@@ -1,59 +1,67 @@
-require 'test_helper'
+require "test_helper"
 
-class Api::V1::StatisticsControllerTest < ActionDispatch::IntegrationTest
-  test "should get success code when call index" do
-    valid_get api_v1_statistics_url
+module Api
+  module V1
+    class StatisticsControllerTest < ActionDispatch::IntegrationTest
+      setup do
+        CkbSync::Api.any_instance.stubs(:get_tip_block_number).returns(100)
+      end
 
-    assert_response :success
-  end
+      test "should get success code when call index" do
+        valid_get api_v1_statistics_url
 
-  test "should set right content type when call index" do
-    valid_get api_v1_statistics_url
+        assert_response :success
+      end
 
-    assert_equal "application/vnd.api+json", response.content_type
-  end
+      test "should set right content type when call index" do
+        valid_get api_v1_statistics_url
 
-  test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
-    get api_v1_statistics_url, headers: { "Content-Type": "text/plain" }
+        assert_equal "application/vnd.api+json", response.content_type
+      end
 
-    assert_equal 415, response.status
-  end
+      test "should respond with 415 Unsupported Media Type when Content-Type is wrong" do
+        get api_v1_statistics_url, headers: { "Content-Type": "text/plain" }
 
-  test "should respond with error object when Content-Type is wrong" do
-    error_object = Api::V1::Exceptions::WrongContentTypeError.new
-    response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+        assert_equal 415, response.status
+      end
 
-    get api_v1_statistics_url, headers: { "Content-Type": "text/plain" }
+      test "should respond with error object when Content-Type is wrong" do
+        error_object = Api::V1::Exceptions::WrongContentTypeError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
-    assert_equal response_json, response.body
-  end
+        get api_v1_statistics_url, headers: { "Content-Type": "text/plain" }
 
-  test "should respond with 406 Not Acceptable when Accept is wrong" do
-    get api_v1_statistics_url, headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+        assert_equal response_json, response.body
+      end
 
-    assert_equal 406, response.status
-  end
+      test "should respond with 406 Not Acceptable when Accept is wrong" do
+        get api_v1_statistics_url, headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
 
-  test "should respond with error object when Accept is wrong" do
-    error_object = Api::V1::Exceptions::WrongAcceptError.new
-    response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
+        assert_equal 406, response.status
+      end
 
-    get api_v1_statistics_url, headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
+      test "should respond with error object when Accept is wrong" do
+        error_object = Api::V1::Exceptions::WrongAcceptError.new
+        response_json = RequestErrorSerializer.new([error_object], message: error_object.title).serialized_json
 
-    assert_equal response_json, response.body
-  end
+        get api_v1_statistics_url, headers: { "Content-Type": "application/vnd.api+json", "Accept": "application/json" }
 
-  test "the returned statistic info should contain right keys" do
-    valid_get api_v1_statistics_url
+        assert_equal response_json, response.body
+      end
 
-    assert_equal %w(average_block_time average_difficulty hash_rate tip_block_number), json.dig("data", "attributes").keys.sort
-  end
+      test "the returned statistic info should contain right keys" do
+        valid_get api_v1_statistics_url
 
-  test "should return right statistic info" do
-    statistic_info = StatisticInfo.new
+        assert_equal %w(average_block_time average_difficulty hash_rate tip_block_number), json.dig("data", "attributes").keys.sort
+      end
 
-    valid_get api_v1_statistics_url
+      test "should return right statistic info" do
+        statistic_info = StatisticInfo.new
 
-    assert_equal IndexStatisticSerializer.new(statistic_info).serialized_json, response.body
+        valid_get api_v1_statistics_url
+
+        assert_equal IndexStatisticSerializer.new(statistic_info).serialized_json, response.body
+      end
+    end
   end
 end
--- server/test/models/statistic_info_test.rb
+++ server/test/models/statistic_info_test.rb
@@ -4,37 +4,37 @@ class StatisticInfoTest < ActiveSupport::TestCase
   test "should has default difficulty interval" do
     statistic_info = StatisticInfo.new
 
-    assert_not_nil statistic_info.difficulty_interval
+    assert_not_nil statistic_info.instance_variable_get(:@difficulty_interval)
   end
 
   test "should has default block time interval" do
     statistic_info = StatisticInfo.new
 
-    assert_not_nil statistic_info.block_time_interval
+    assert_not_nil statistic_info.instance_variable_get(:@block_time_interval)
   end
 
   test "should has default statistical interval" do
     statistic_info = StatisticInfo.new
 
-    assert_not_nil statistic_info.statistical_interval
+    assert_not_nil statistic_info.instance_variable_get(:@statistical_interval)
   end
 
   test "the default difficulty interval should equal to env config" do
     statistic_info = StatisticInfo.new
 
-    assert_equal ENV["STATISTICAL_INTERVAL"], statistic_info.difficulty_interval
+    assert_equal ENV["STATISTICAL_INTERVAL"], statistic_info.instance_variable_get(:@difficulty_interval)
   end
 
   test "the default block time interval should equal to env config" do
     statistic_info = StatisticInfo.new
 
-    assert_equal ENV["STATISTICAL_INTERVAL"], statistic_info.block_time_interval
+    assert_equal ENV["STATISTICAL_INTERVAL"], statistic_info.instance_variable_get(:@block_time_interval)
   end
 
   test "the default statistical interval should equal to env config" do
     statistic_info = StatisticInfo.new
 
-    assert_equal ENV["STATISTICAL_INTERVAL"], statistic_info.statistical_interval
+    assert_equal ENV["STATISTICAL_INTERVAL"], statistic_info.instance_variable_get(:@statistical_interval)
   end
 
   test "id should present" do
@@ -48,5 +48,24 @@ class StatisticInfoTest < ActiveSupport::TestCase
     CkbSync::Api.any_instance.expects(:get_tip_block_number).returns(100)
 
     assert_equal 100, statistic_info.tip_block_number
+  end
+
+  test ".average_difficulty should return average of the last 500 blocks if use default average interval" do
+    statistic_info = StatisticInfo.new
+    create_list(:block, 500, :with_block_hash)
+    last_500_blocks = Block.recent.take(ENV["STATISTICAL_INTERVAL"])
+    average_difficulty = last_500_blocks.map { |block| block.difficulty.hex }.reduce(&:+) / ENV["STATISTICAL_INTERVAL"].to_i
+
+    assert_equal average_difficulty, statistic_info.average_difficulty
+  end
+
+  test ".average_difficulty should return average of the total blocks if blocks count less then default average interval" do
+    statistic_info = StatisticInfo.new
+    block_count = 10
+    create_list(:block, block_count, :with_block_hash)
+    last_10_blocks = Block.recent.take(block_count)
+    average_difficulty = last_10_blocks.map { |block| block.difficulty.hex }.reduce(&:+) / block_count
+
+    assert_equal average_difficulty, statistic_info.average_difficulty
   end
 end
