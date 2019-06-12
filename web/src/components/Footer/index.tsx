86fe558847ee665381369c5366be19f087c96530
duanyytop <duanyytop@gmail.com>
2019-06-12 17:34:25 +0800

    feat(ui): adapt footer for mobile

--- web/src/components/Footer/index.tsx
+++ web/src/components/Footer/index.tsx
@@ -23,43 +23,46 @@ const FooterDiv = styled.div`
   background-color: #424242;
   display: flex;
   flex-direction: column;
-  a {
-    text-decoration: none;
-  }
-  .footer__top,
-  .footer__bottom {
+
+  .footer__top {
     display: flex;
     padding: 20px;
     align-items: center;
     justify-content: center;
-  }
-  .footer__top {
     min-height: ${(props: { width: number }) => (380 * props.width) / 1920}px;
+
     .container {
       display: flex;
       flex-wrap: wrap;
-      .footer__top__orgs,
+
       .footer__top__logo {
         display: flex;
         align-items: center;
         justify-content: center;
         margin: 0 auto;
-        width: ${153 * 2 + 121}px;
-      }
-      .footer__top__logo {
+        width: 276px;
+
         img {
           width: ${(props: { width: number }) => (160 * props.width) / 1920}px;
           height: auto;
         }
       }
-      .footer__top__orgs {
+
+      .footer__top__items {
         flex: 1;
-        .footer__top__orgs__item {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        margin: 0 auto;
+        width: 276px;
+
+        .footer__top__item {
           opacity: 0.8;
           display: flex;
           align-items: center;
           margin-top: 25px;
           margin-bottom: 25px;
+
           > div:nth-child(1) {
             font-size: 16px;
             width: 90px;
@@ -67,48 +70,83 @@ const FooterDiv = styled.div`
             margin-right: 10px;
             color: #3cc68a;
           }
+
           > div:nth-child(2) {
             flex: 1;
             display: flex;
             flex-wrap: wrap;
           }
-          .footer__top__orgs__item__link {
-            margin-left: 15px;
-            margin-top: 15px;
-            display: flex;
-            flex-direction: column;
-            align-items: center;
-            text-decoration: none;
 
-            >div: nth-child(1) {
-              width: 32px;
-              height: 32px;
-              img {
-                width: 100%;
-                height: auto;
-              }
-            }
-            >div: nth-child(2) {
-              color: white;
-              font-size: 12px;
-              margin-top: 7px;
-              text-align: center;
+          @media (max-width: 700px) {
+            margin-top: 15px;
+            margin-bottom: 15px;
+
+            > div:nth-child(1) {
+              font-size: 14px;
+              width: 80px;
+              margin-top: 15px;
             }
           }
         }
       }
     }
   }
-  .footer__bottom {
+
+  .footer__copyright {
+    display: flex;
+    padding: 20px;
+    align-items: center;
+    justify-content: center;
     min-height: ${(props: { width: number }) => (102 * props.width) / 1920}px;
     border-top: 1px solid white;
     font-size: 16px;
     line-height: 22px;
     text-align: center;
     color: #e3e3e3;
+
+    @media (max-width: 700px) {
+      padding: 10px;
+      font-size: 12px;
+    }
   }
 `
-const orgs = [
+
+const FooterItemPanel = styled.a`
+  margin-left: 15px;
+  margin-top: 15px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-decoration: none;
+
+  >div: nth-child(1) {
+    width: 32px;
+    height: 32px;
+    img {
+      width: 100%;
+      height: auto;
+    }
+  }
+  >div: nth-child(2) {
+    color: white;
+    font-size: 12px;
+    margin-top: 7px;
+    text-align: center;
+  }
+
+  @media (max-width: 700px) {
+    >div: nth-child(1) {
+      width: 20px;
+      height: 20px;
+    }
+    >div: nth-child(2) {
+      font-size: 10px;
+      margin-top: 5px;
+    }
+  }
+`
+
+const footers = [
   {
     name: 'Nervos Foundation',
     items: [
@@ -181,6 +219,17 @@ const orgs = [
   },
 ]
 
+const FooterItem = (link: any) => {
+  return (
+    <FooterItemPanel key={link.label} href={link.url} rel="noopener noreferrer" target="_blank">
+      <div>
+        <img src={link.icon} alt="orgItemLogo" />
+      </div>
+      <div>{link.label}</div>
+    </FooterItemPanel>
+  )
+}
+
 export default () => {
   return (
     <FooterDiv width={window.innerWidth}>
@@ -191,28 +240,15 @@ export default () => {
               <img src={LogoIcon} alt="logo" />
             </Link>
           </div>
-          <div className="footer__top__orgs">
+          <div className="footer__top__items">
             <div>
-              {orgs.map((item: any) => {
+              {footers.map((item: any) => {
                 return (
-                  <div key={item.name} className="footer__top__orgs__item">
+                  <div key={item.name} className="footer__top__item">
                     <div>{item.name}</div>
                     <div>
                       {item.items.map((link: any) => {
-                        return (
-                          <a
-                            key={link.label}
-                            href={link.url}
-                            rel="noopener noreferrer"
-                            target="_blank"
-                            className="footer__top__orgs__item__link"
-                          >
-                            <div>
-                              <img src={link.icon} alt="orgItemLogo" />
-                            </div>
-                            <div>{link.label}</div>
-                          </a>
-                        )
+                        return FooterItem(link)
                       })}
                     </div>
                   </div>
@@ -222,7 +258,7 @@ export default () => {
           </div>
         </div>
       </div>
-      <div className="footer__bottom">{`Copyright © ${getCurrentYear()} Nervos Foundation. All Rights Reserved.`}</div>
+      <div className="footer__copyright">{`Copyright © ${getCurrentYear()} Nervos Foundation. All Rights Reserved.`}</div>
     </FooterDiv>
   )
 }
