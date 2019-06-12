476cced31c7e523bd1841569ca71309b6566cc1b
duanyytop <duanyytop@gmail.com>
2019-06-12 18:00:08 +0800

    feat(ui): add epoch prefix for start number and length

--- web/src/pages/BlockDetail/index.tsx
+++ web/src/pages/BlockDetail/index.tsx
@@ -300,12 +300,12 @@ export default (props: React.PropsWithoutRef<RouteComponentProps<{ hash: string 
     },
     {
       image: StartNumberIcon,
-      label: 'Start Number:',
+      label: 'Epoch Start Number:',
       value: `${blockData.start_number}`,
     },
     {
       image: LengthIcon,
-      label: 'Length:',
+      label: 'Epoch Length:',
       value: blockData.length,
     },
     {
