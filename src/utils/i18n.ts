900b427ef4c15c80202a3b62a61ac2897b552068
Xiaolu <lfp@cryptape.com>
2019-06-28 17:43:10 +0800

    chore: zh.json i18n

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -3,16 +3,26 @@ import { initReactI18next } from 'react-i18next'
 import zh from '../locales/zh.json'
 import en from '../locales/en.json'
 
+const chooseLocale = () => {
+  switch (navigator.language.split('-')[0]) {
+    case 'en':
+      return 'en'
+    case 'zh':
+      return 'zh'
+    default:
+      return 'en'
+  }
+}
+
 i18n.use(initReactI18next).init({
   resources: {
     en,
     zh,
   },
-  fallbackLng: 'en',
+  fallbackLng: chooseLocale(),
   interpolation: {
     escapeValue: false,
   },
-  debug: true,
 })
 
 export default i18n
