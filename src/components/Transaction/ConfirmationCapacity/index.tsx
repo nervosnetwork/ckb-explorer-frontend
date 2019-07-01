e534ec2d44cdc7fddba582c0bee199e98110836f
cezres <cezr@sina.com>
2019-07-01 10:31:50 +0800

    feat: Localized capacity number

--- src/components/Transaction/ConfirmationCapacity/index.tsx
+++ src/components/Transaction/ConfirmationCapacity/index.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Container from './styled'
 import { formattorConfirmation, shannonToCkb } from '../../../utils/util'
+import { localeNumberString } from '../../../utils/number'
 
 export default ({ confirmation, capacity }: { confirmation?: number; capacity: number }) => {
   return (
     <Container increased={false}>
       {confirmation !== undefined && <div className="confirmation">{formattorConfirmation(confirmation)}</div>}
-      <div className="capacity">{`${capacity >= 0 ? '+' : '-'} ${shannonToCkb(Math.abs(capacity))} CKB`}</div>
+      <div className="capacity">
+        {`${capacity >= 0 ? '+' : '-'} ${localeNumberString(shannonToCkb(Math.abs(capacity)))} CKB`}
+      </div>
     </Container>
   )
 }
