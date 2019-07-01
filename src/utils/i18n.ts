5187e9903ae9250c3623c2314dd4d32b68371f3c
Xiaolu <lfp@cryptape.com>
2019-07-01 16:08:03 +0800

    fix: remove zh i18n

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,14 +1,12 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import en from '../locales/en.json'
-import zh from '../locales/zh.json'
 
 i18n.use(initReactI18next).init({
   resources: {
     en,
-    zh,
   },
-  fallbackLng: 'zh',
+  fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
   },
