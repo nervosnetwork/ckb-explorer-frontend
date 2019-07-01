6f09d2ea0606070b7fe21e975f3a72f608c1bf1d
Xiaolu <lfp@cryptape.com>
2019-07-01 14:53:14 +0800

    chore: set default language to en

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,25 +1,12 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
-import zh from '../locales/zh.json'
 import en from '../locales/en.json'
-
-const chooseLocale = () => {
-  switch (navigator.language.split('-')[0]) {
-    case 'en':
-      return 'en'
-    case 'zh':
-      return 'zh'
-    default:
-      return 'en'
-  }
-}
 
 i18n.use(initReactI18next).init({
   resources: {
     en,
-    zh,
   },
-  fallbackLng: chooseLocale(),
+  fallbackLng: 'en',
   interpolation: {
     escapeValue: false,
   },
