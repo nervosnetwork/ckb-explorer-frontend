963eb761784f27f341efca611db44318b0a6a14e
cezres <cezr@sina.com>
2019-07-01 17:00:22 +0800

    fix: Modify `TransactionConfirmation`

--- src/components/Transaction/TransactionConfirmation/index.tsx
+++ src/components/Transaction/TransactionConfirmation/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import Container from './styled'
-import { formattorConfirmation, shannonToCkb } from '../../../utils/util'
+import { shannonToCkb, formatConfirmation } from '../../../utils/util'
 import { localeNumberString } from '../../../utils/number'
 
 export default ({ confirmation, capacity }: { confirmation?: number; capacity: number }) => {
   return (
     <Container increased={false}>
-      {confirmation !== undefined && <div className="confirmation">{formattorConfirmation(confirmation)}</div>}
+      {confirmation !== undefined && <div className="confirmation">{formatConfirmation(confirmation)}</div>}
       <div className="capacity">
         {`${capacity >= 0 ? '+' : '-'} ${localeNumberString(shannonToCkb(Math.abs(capacity)))} CKB`}
       </div>
