ff4890255383b937aea9e5b65a665602890d581b
duanyytop <duanyytop@gmail.com>
2019-07-09 15:16:23 +0800

    chore: update 404 page

--- src/pages/404/index.tsx
+++ src/pages/404/index.tsx
@@ -1,24 +1,25 @@
 import React from 'react'
 import styled from 'styled-components'
 import Content from '../../components/Content'
-import NotFoundIcon from '../../assets/not_found_404.png'
+import NotFoundIcon from '../../assets/404.png'
 
 const NotFoundPanel = styled.div`
   width: 100%;
-  margin-top: 127px;
-  margin-bottom: 105px;
+  margin-top: 120px;
+  margin-bottom: 140px;
 `
 
 const NotFoundImage = styled.img`
   width: 100%;
   height: auto;
-  max-width: 757px;
-  max-height: 357px;
+  width: 700px;
+  height: 386px;
   display: block;
   margin: 0 auto;
 
   @media (max-width: 700px) {
-    width: 80%;
+    width: 216px;
+    height: 120px;
   }
 `
 
