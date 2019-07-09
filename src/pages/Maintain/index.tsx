04ab19ad95cbe12f8ae64be231379f1999ccee2e
duanyytop <duanyytop@gmail.com>
2019-07-09 14:21:27 +0800

    chore: update pc maintain page

--- src/pages/Maintain/index.tsx
+++ src/pages/Maintain/index.tsx
@@ -4,20 +4,29 @@ import styled from 'styled-components'
 import { AxiosResponse } from 'axios'
 import AppContext from '../../contexts/App'
 import { axiosIns } from '../../http/fetcher'
+import MaintainImage from '../../assets/maintain.png'
 
 const MaintainPanel = styled.div`
   width: 100%;
   overflow-x: hidden;
   height: 100%;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
 
+  > img {
+    width: 750px;
+    hieght: 364px;
+    display: block;
+    margin-top: 220px;
+  }
+
   > div {
-    font-size: 36px;
+    font-size: 26px;
     font-weight: bold;
-    color: #3cc68a;
-    margin-top: 20%;
+    color: #606060;
+    margin-bottom: 250px;
 
     @media (max-width: 700px) {
       margin-top: 40%;
@@ -29,7 +38,7 @@ const MaintainPanel = styled.div`
 
 const fetchTipBlockNumber = (replace: any) => {
   return axiosIns
-    .get('statistics/tip_block_number')
+    .get('statistics/tip_block_numbe')
     .then((res: AxiosResponse) => {
       if (res.status === 200) {
         replace('/')
@@ -51,6 +60,7 @@ export default (props: React.PropsWithoutRef<RouteComponentProps>) => {
 
   return (
     <MaintainPanel>
+      <img src={MaintainImage} alt="maintain" />
       <div>{appContext.errorMessage}</div>
     </MaintainPanel>
   )
