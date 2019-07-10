f78b1bb95a5753bd0a75dfd146493a249262002f
duanyytop <duanyytop@gmail.com>
2019-07-10 16:02:10 +0800

    fix: fix lockhash page address null bug

--- src/pages/Address/index.tsx
+++ src/pages/Address/index.tsx
@@ -306,14 +306,14 @@ export default (props: React.PropsWithoutRef<RouteComponentProps<{ address: stri
                   transaction &&
                   (isMobile() ? (
                     <TransactionCard
-                      address={address}
+                      address={state.address.address_hash}
                       confirmation={state.tipBlockNumber - transaction.attributes.block_number + 1}
                       transaction={transaction.attributes}
                       key={transaction.attributes.transaction_hash}
                     />
                   ) : (
                     <TransactionItem
-                      address={address}
+                      address={state.address.address_hash}
                       transaction={transaction.attributes}
                       confirmation={state.tipBlockNumber - transaction.attributes.block_number + 1}
                       key={transaction.attributes.transaction_hash}
