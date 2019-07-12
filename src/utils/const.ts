409334a4c2a20e6a396abef73ed0777df98ad231
duanyytop <duanyytop@gmail.com>
2019-07-12 11:49:43 +0800

    chore: update blockchain alert polling time

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,6 @@
 export const BLOCK_POLLING_TIME = 4000
 export const MAX_CONFIRMATION = 1000
-export const BLOCKCHAIN_ALERT_POLLING_TIME = 2000
+export const BLOCKCHAIN_ALERT_POLLING_TIME = 10000
 
 export const CachedKeys = {
   // Block List
