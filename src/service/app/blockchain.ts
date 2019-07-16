d863919774a4cc6db1398126ea83070098817929
duanyytop <duanyytop@gmail.com>
2019-07-16 14:41:51 +0800

    refactor: refactor service http and app

---
+++ src/service/app/blockchain.ts
@@ -0,0 +1,34 @@
+import { Wrapper } from '../http/Response'
+import { fetchBlockchainInfo } from '../http/fetcher'
+
+const alertNotEmpty = (response: Wrapper<State.BlockchainInfo>): boolean => {
+  return (
+    response &&
+    response.attributes &&
+    response.attributes.blockchain_info &&
+    response.attributes.blockchain_info.alerts &&
+    response.attributes.blockchain_info.alerts.length > 0
+  )
+}
+
+export const handleBlockchainAlert = (appContext: any) => {
+  fetchBlockchainInfo().then((response: Wrapper<State.BlockchainInfo>) => {
+    if (alertNotEmpty(response)) {
+      appContext.updateAppErrors({
+        type: 'ChainAlert',
+        message: response.attributes.blockchain_info.alerts.map(alert => {
+          return alert.message
+        }),
+      })
+    } else {
+      appContext.updateAppErrors({
+        type: 'ChainAlert',
+        message: [],
+      })
+    }
+  })
+}
+
+export default {
+  handleBlockchainAlert,
+}
