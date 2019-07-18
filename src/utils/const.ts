082edd5dc35e69f4e73cb42bb9c3bd6f68c73480
duanyytop <duanyytop@gmail.com>
2019-07-18 14:40:32 +0800

    feat: add uri related method

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -3,6 +3,10 @@ export const MAX_CONFIRMATION = 1000
 export const BLOCKCHAIN_ALERT_POLLING_TIME = 10000
 export const RESIZE_LATENCY = 500
 
+export const HttpErrorCode = {
+  NOT_FOUND_ADDRESS: 1010,
+}
+
 export const CachedKeys = {
   // Block List
   BlockList: 'block_list',
