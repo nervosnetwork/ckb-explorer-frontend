5931b16cf1aaadb25730d71e5fabd5766c1eda7f
cezres <cezr@sina.com>
2019-07-18 17:22:56 +0800

    chore: Add isMediumMobile function

--- src/utils/screen.ts
+++ src/utils/screen.ts
@@ -2,10 +2,13 @@ export const isMobile = () => window.innerWidth <= 700
 
 export const isSmallMobile = () => window.innerWidth <= 320
 
-export const isLargeMobile = () => window.innerWidth <= 414
+export const isMediumMobile = () => window.innerWidth <= 375
+
+export const isLargeMobile = () => window.innerWidth <= 414 || isMobile()
 
 export default {
   isMobile,
   isSmallMobile,
+  isMediumMobile,
   isLargeMobile,
 }
