37b8698c4ce8ff58c9be07f5c206ae9b731a4b37
Xiaolu <lfp@cryptape.com>
2019-07-23 17:09:59 +0800

    chore: mobile page

--- src/components/Pagination/styled.tsx
+++ src/components/Pagination/styled.tsx
@@ -5,6 +5,9 @@ export const BasePagePanel = styled.div`
   background: white;
   width: 100%;
   height: 80px;
+  @media (max-width: 700px) {
+    margin-left: 6%;
+  }
 `
 
 export const PageInput = styled.input`
@@ -21,6 +24,7 @@ export const PageInput = styled.input`
 
 export const PaginationItem = styled.div`
   display: flex;
+  width: 100%;
   font-size: 16px;
   text-align: center;
   color: #000000;
@@ -35,6 +39,10 @@ export const PaginationItem = styled.div`
     outline: none;
     cursor: pointer;
     background: #f5f5f5;
+    > img {
+      width: 12px;
+      height: 20px;
+    }
   }
 
   .first {
@@ -52,10 +60,6 @@ export const PaginationItem = styled.div`
     margin-top: 20px;
     cursor: ${(props: { isFirstPage: boolean; isLastPage: boolean }) =>
       props.isFirstPage ? 'not-allowed' : 'pointer'};
-    > img {
-      width: 12px;
-      height: 20px;
-    }
   }
 
   .middleLabel {
@@ -72,10 +76,6 @@ export const PaginationItem = styled.div`
     margin-left: 20px;
     margin-top: 20px;
     cursor: ${(props: { isFirstPage: boolean; isLastPage: boolean }) => (props.isLastPage ? 'not-allowed' : 'pointer')};
-    > img {
-      width: 12px;
-      height: 20px;
-    }
   }
 
   .last {
@@ -99,5 +99,32 @@ export const PaginationItem = styled.div`
     font-size: 16px;
     line-height: 80px;
     margin-left: 190px;
+  }
+
+  @media (max-width: 700px) {
+    width: 88%;
+    height: 50px;
+
+    > button {
+      height: 20px;
+      > img {
+        width: 6px;
+        height: 10px;
+      }
+    }
+
+    .first {
+      display: none;
+    }
+
+    .last {
+      display: none;
+    }
+
+    .leftImage {
+      width: 16px;
+      margin-top: 15px;
+      margin-left: 20px;
+    }
   }
 `
