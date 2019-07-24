a6e1275a619f95934c52414b309d09b24627c3c1
cezres <cezr@sina.com>
2019-07-24 18:53:46 +0800

    feat: OverviewCard adaptation mobile

--- src/components/Card/OverviewCard/index.tsx
+++ src/components/Card/OverviewCard/index.tsx
@@ -29,9 +29,25 @@ const handleOverviewItems = (items: OverviewItemData[]) => {
   }
 }
 
-const OverviewItem = ({ title, content }: { title?: ReactNode; content?: ReactNode }) => {
+const isValidReactNode = (node: ReactNode) => {
+  if (node === undefined || node === null) return false
+  if (node instanceof Array) {
+    return node.reduce((current, item) => (current ? true : !!item))
+  }
+  return true
+}
+
+const OverviewItem = ({
+  title,
+  content,
+  hiddenLine,
+}: {
+  title?: ReactNode
+  content?: ReactNode
+  hiddenLine: boolean
+}) => {
   return (
-    <OverviewItemPanel>
+    <OverviewItemPanel hiddenLine={hiddenLine}>
       <div className="overview_item__title">{title}</div>
       <div className="overview_item__value">{content}</div>
     </OverviewItemPanel>
@@ -44,14 +60,24 @@ export default ({ items, children }: { items: OverviewItemData[]; children?: Rea
     <OverviewCardPanel>
       <OverviewContentPanel length={leftItems.length}>
         <div className="overview_content__left_items">
-          {leftItems.map(item => (
-            <OverviewItem key={items.indexOf(item)} title={item.title} content={item.content} />
+          {leftItems.map((item, idx) => (
+            <OverviewItem
+              key={items.indexOf(item)}
+              title={item.title}
+              content={item.content}
+              hiddenLine={!isValidReactNode(children) && idx === leftItems.length - 1}
+            />
           ))}
         </div>
         <span />
         <div className="overview_content__right_items">
-          {rightItems.map(item => (
-            <OverviewItem key={items.indexOf(item)} title={item.title} content={item.content} />
+          {rightItems.map((item, idx) => (
+            <OverviewItem
+              key={items.indexOf(item)}
+              title={item.title}
+              content={item.content}
+              hiddenLine={!isValidReactNode(children) && idx === rightItems.length - 1}
+            />
           ))}
         </div>
       </OverviewContentPanel>
