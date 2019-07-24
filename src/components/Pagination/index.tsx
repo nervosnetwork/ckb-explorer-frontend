882edf25f9e911c8379b1619a56174f4eaeb9380
Xiaolu <lfp@cryptape.com>
2019-07-24 15:56:30 +0800

    fix: modify image's file name

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,51 +1,49 @@
 import React, { useState } from 'react'
 import { PaginationLeftItem, PaginationRightItem, PaginationPanel } from './styled'
-import LeftBlack from '../../assets/pagination-black-left.png'
-import RightBlack from '../../assets/pagination-black-right.png'
-import LeftGreen from '../../assets/pagination-green-left.png'
-import RightGreen from '../../assets/pagination-green-right.png'
+import LeftBlack from '../../assets/pagination_black_left.png'
+import RightBlack from '../../assets/pagination_black_right.png'
+import LeftGreen from '../../assets/pagination_green_left.png'
+import RightGreen from '../../assets/pagination_green_right.png'
 import i18n from '../../utils/i18n'
-
-const pageSize: number = 25
 
 const PageFirstItem = ({
   currentPage,
   total,
-  defautInput,
+  defautJumpPage,
   onChange,
 }: {
   currentPage: number
   total: number
-  defautInput: string
-  onChange: (page: number, pageSize: number) => void
+  defautJumpPage: string
+  onChange: (page: number) => void
 }) => {
-  const [inputValue, setInputValue] = useState(defautInput || '')
+  const [inputValue, setInputValue] = useState(defautJumpPage || '')
 
   const goFirstPage = () => {
     const page: number = 1
-    onChange(page, pageSize)
+    onChange(page)
   }
 
   const goLastPage = () => {
-    onChange(total, pageSize)
+    onChange(total)
   }
 
   const goPrev = () => {
-    if (currentPage !== 1) {
-      onChange(currentPage - 1, pageSize)
+    if (currentPage !== 1 && currentPage > 0) {
+      onChange(currentPage - 1)
     }
   }
 
   const goNext = () => {
     if (currentPage < total) {
-      onChange(currentPage + 1, pageSize)
+      onChange(currentPage + 1)
     }
   }
 
   const jumpPage = () => {
     const inputValueNumber = Number(inputValue)
     if (!Number.isNaN(inputValueNumber) && inputValueNumber <= total && inputValueNumber >= 1) {
-      onChange(inputValueNumber, pageSize)
+      onChange(inputValueNumber)
     }
   }
 
@@ -63,16 +61,16 @@ const PageFirstItem = ({
         <button type="button" className="first" onClick={() => goFirstPage()}>
           {i18n.t('pagination.first')}
         </button>
-        <button type="button" className="leftImage" onClick={() => goPrev()}>
+        <button type="button" className="leftimage" onClick={() => goPrev()}>
           <img src={isFirstPage() ? LeftBlack : LeftGreen} alt="left" />
         </button>
-        <div className="middleLabel">
+        <div className="middlelabel">
           Page
           {currentPage}
           of
           {total}
         </div>
-        <button type="button" className="rightImage" onClick={() => goNext()}>
+        <button type="button" className="rightimage" onClick={() => goNext()}>
           <img src={isLastPage() ? RightBlack : RightGreen} alt="right" />
         </button>
         <button type="button" className="last" onClick={() => goLastPage()}>
@@ -105,7 +103,7 @@ export default ({
 }: {
   currentPage: number
   total: number
-  onChange: (page: number, pageSize: number) => void
+  onChange: (page: number) => void
 }) => {
-  return <PageFirstItem currentPage={currentPage} total={total} defautInput="1" onChange={onChange} />
+  return <PageFirstItem currentPage={currentPage} total={total} defautJumpPage="1" onChange={onChange} />
 }
