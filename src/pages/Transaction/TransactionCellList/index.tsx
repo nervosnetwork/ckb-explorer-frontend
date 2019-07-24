f11eb8a4616b99fece228303b79a9ad9321431ac
cezres <cezr@sina.com>
2019-07-24 18:51:41 +0800

    feat: TransactionCellList adaptation mobile

--- src/pages/Transaction/TransactionCellList/index.tsx
+++ src/pages/Transaction/TransactionCellList/index.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import { TransactionCellListPanel, TransactionCellListTitlePanel } from './styled'
 import TransactionCell from '../TransactionCell'
+import { CellType } from '../TransactionDetail'
 
-export default () => {
+export default ({ inputs, outputs }: { inputs?: State.InputOutput[]; outputs?: State.InputOutput[] }) => {
+  const cells = inputs || outputs || []
   return (
     <TransactionCellListPanel>
       <TransactionCellListTitlePanel>
         <div className="transaction__cell_list_titles">
-          <span>Input</span>
+          <span>{inputs ? 'Input' : 'Output'}</span>
           <span>Capacity</span>
           <span>Detail</span>
         </div>
       </TransactionCellListTitlePanel>
-      <TransactionCell />
+      {cells.map(cell => (
+        <TransactionCell cell={cell} cellType={inputs ? CellType.Input : CellType.Output} />
+      ))}
     </TransactionCellListPanel>
   )
 }
--- src/pages/Transaction/TransactionCellList/styled.tsx
+++ src/pages/Transaction/TransactionCellList/styled.tsx
@@ -4,6 +4,10 @@ export const TransactionCellListTitlePanel = styled.div`
   display: flex;
   flex-direction: column;
 
+  @media (max-width: 700px) {
+    display: none;
+  }
+
   .transaction__cell_list_titles {
     display: flex;
     flex-direction: row;
@@ -36,10 +40,16 @@ export const TransactionCellListTitlePanel = styled.div`
 
 export const TransactionCellListPanel = styled.div`
   width: 100%;
-  border-radius: 6px;
-  box-shadow: 2px 2px 6px 0 #dfdfdf;
-  background-color: #ffffff;
-  margin-top: 20px;
 
-  padding: 20px 40px 20px 40px;
+  @media (min-width: 700px) {
+    border-radius: 6px;
+    box-shadow: 2px 2px 6px 0 #dfdfdf;
+    background-color: #ffffff;
+    margin-top: 20px;
+    padding: 20px 40px 20px 40px;
+  }
+
+  @media (max-width: 700px) {
+    margin-top: 5px;
+  }
 `
