bfbfdc2d0d0b903d24c78de8d537a8c1190a0cc6
duanyytop <duanyytop@gmail.com>
2019-07-30 09:59:34 +0800

    fix: add placeholder when search input focus lost

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -144,6 +144,7 @@ const Search = ({ dispatch, hasBorder, content }: { dispatch: AppDispatch; hasBo
         defaultValue={searchValue || ''}
         hasBorder={!!hasBorder}
         onFocus={() => setPlaceholder('')}
+        onBlur={() => setPlaceholder(SearchPlaceholder)}
         onChange={(event: any) => {
           if (!event.target.value) {
             setPlaceholder(SearchPlaceholder)
