7112808841288c73ad022787dece3bfcfb941411
duanyytop <duanyytop@gmail.com>
2019-08-07 17:49:11 +0800

    fix: move View All to i18n file

--- src/components/TransactionItem/TransactionItemCellList/index.tsx
+++ src/components/TransactionItem/TransactionItemCellList/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react'
 import { Link } from 'react-router-dom'
 import TransactionCellListPanel from './styled'
+import i18n from '../../../utils/i18n'
 
 interface TransactionCellListProps {
   cells: State.InputOutput[]
@@ -15,7 +16,7 @@ export default ({ cells, showSize, transaction, render }: TransactionCellListPro
       {cells && cells.map((cell, index) => index < showSize && render(cell))}
       {cells && showSize < cells.length && (
         <div className="transaction_item__view_all">
-          <Link to={`/transaction/${transaction.transactionHash}`}>View All</Link>
+          <Link to={`/transaction/${transaction.transactionHash}`}>{i18n.t('common.view_all')}</Link>
         </div>
       )}
     </TransactionCellListPanel>
