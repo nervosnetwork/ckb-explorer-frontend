743d2b78d1ac936b81b8cfe3838cfe2f8f2f137f
duanyytop <duanyytop@gmail.com>
2019-08-07 10:54:14 +0800

    chore: remove no use util method

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,32 +1,3 @@
-export const parseHashRate = (value: number) => {
-  const kv = value / 1000
-  const mv = kv / 1000
-  const gv = mv / 1000
-  const tv = gv / 1000
-  const pv = tv / 1000
-  const ev = pv / 1000
-
-  if (ev >= 1) {
-    return `${ev.toFixed(2)} EH/s`
-  }
-  if (pv >= 1) {
-    return `${pv.toFixed(2)} PH/s`
-  }
-  if (tv >= 1) {
-    return `${tv.toFixed(2)} TH/s`
-  }
-  if (gv >= 1) {
-    return `${gv.toFixed(2)} GH/s`
-  }
-  if (mv >= 1) {
-    return `${mv.toFixed(2)} MH/s`
-  }
-  if (kv >= 1) {
-    return `${kv.toFixed(2)} KH/s`
-  }
-  return `${value.toFixed(2)} H/s`
-}
-
 export const parseNumber = (value: number | string, radix?: number) => {
   if (typeof value === 'string') {
     if (radix) {
@@ -52,7 +23,6 @@ export const localeNumberString = (value: number | string, radix?: number) => {
 }
 
 export default {
-  parseHashRate,
   parseNumber,
   localeNumberString,
 }
--- tests/number.test.ts
+++ tests/number.test.ts
@@ -2,11 +2,6 @@ import { parseHashRate, parseNumber, localeNumberString } from '../src/utils/num
 
 describe('Number methods tests', () => {
 
-  it('parse hash rate', async () => {
-    expect(parseHashRate(1234546698945)).toBe("1.23 TH/s")
-    expect(parseHashRate(100003439)).toBe("100.00 MH/s")
-  })
-
   it('parse number', async () => {
     expect(parseNumber('222333')).toBe(222333)
     expect(parseNumber('222333', 16)).toBe(2237235)
