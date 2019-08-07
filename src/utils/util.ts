5b029f60424314c82eacacaed45dae07341c24a3
duanyytop <duanyytop@gmail.com>
2019-08-07 10:15:19 +0800

    refactor: move capacity handle methods from util to component

--- src/utils/util.ts
+++ src/utils/util.ts
@@ -1,7 +1,6 @@
 import { ReactNode } from 'react'
 import { MAX_CONFIRMATION } from './const'
 import i18n from './i18n'
-import { parseNumber } from './number'
 
 export const copyElementValue = (component: any) => {
   if (!component) return
@@ -18,23 +17,6 @@ export const copyElementValue = (component: any) => {
 
 export const shannonToCkb = (value: number) => {
   return value / 10 ** 8
-}
-
-const handleCellCapacity = (cells: State.InputOutput[], address?: string) => {
-  if (!cells || cells.length === 0) return 0
-  return cells
-    .filter((cell: State.InputOutput) => cell.address_hash === address)
-    .map((cell: State.InputOutput) => parseNumber(cell.capacity))
-    .reduce((previous: number, current: number) => {
-      return previous + current
-    }, 0)
-}
-
-export const handleCapacityChange = (transaction: State.Transaction, address?: string) => {
-  if (!transaction) return 0
-  return (
-    handleCellCapacity(transaction.display_outputs, address) - handleCellCapacity(transaction.display_inputs, address)
-  )
 }
 
 export const formatConfirmation = (confirmation: number | undefined) => {
@@ -61,7 +43,6 @@ export const isValidReactNode = (node: ReactNode) => {
 export default {
   copyElementValue,
   shannonToCkb,
-  handleCapacityChange,
   formatConfirmation,
   isValidReactNode,
 }
