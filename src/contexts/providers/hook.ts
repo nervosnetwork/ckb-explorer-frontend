5ef5ed461c0e785f8bb3679e9c49ff1c8dc361d9
duanyytop <duanyytop@gmail.com>
2019-08-14 10:08:07 +0800

    chore: update language init

--- src/contexts/providers/hook.ts
+++ src/contexts/providers/hook.ts
@@ -6,7 +6,7 @@ import { initNodeVersion } from '../../service/app/nodeInfo'
 import { AppDispatch, AppActions } from './reducer'
 import { fetchCachedData } from '../../utils/cached'
 import { changeLanguage } from '../../utils/i18n'
-import { AppContext } from '.'
+import { AppContext } from './index'
 
 export const useInterval = (callback: () => void, delay: number) => {
   const savedCallback = useRef(() => {})
@@ -53,9 +53,8 @@ export const useInitApp = (dispatch: AppDispatch) => {
     initAxiosInterceptors(dispatch)
     initNodeVersion(dispatch)
 
-    const language =
-      fetchCachedData<'zh' | 'en'>(CachedKeys.AppLanguage) || navigator.language.includes('zh') ? 'zh' : 'en'
-    if (language && language !== app.language) {
+    const language = fetchCachedData<'zh' | 'en'>(CachedKeys.AppLanguage) || app.language
+    if (language) {
       dispatch({
         type: AppActions.UpdateAppLanguage,
         payload: {
