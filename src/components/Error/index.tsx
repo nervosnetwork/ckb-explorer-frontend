f203e4aed3baab75d8c36cb66d43e017675a17e2
duanyytop <duanyytop@gmail.com>
2019-08-17 11:37:08 +0800

    feat: add error page

---
+++ src/components/Error/index.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import styled from 'styled-components'
+import PCDataNotFoundImage from '../../assets/pc_data_not_found.png'
+
+const ErrorPanel = styled.div`
+  margin-top: 60px @media (max-width: 700px) {
+    margin-top: 30px;
+  }
+
+  > img {
+    width: 1038px;
+    height: 480px;
+    margin: 0 auto;
+
+    @media (max-width: 700px) {
+      width: 282px;
+      height: 130px;
+    }
+  }
+`
+
+export default () => {
+  return (
+    <ErrorPanel>
+      <img alt="data not fonund" src={PCDataNotFoundImage} />
+    </ErrorPanel>
+  )
+}
