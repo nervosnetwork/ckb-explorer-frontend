8e135b14f5ad3b8d9d75fabd1971155f56b91f38
duanyytop <duanyytop@gmail.com>
2019-09-06 18:22:26 +0800

    refactor: update state data type

--- src/contexts/states/statistics.ts
+++ src/contexts/states/statistics.ts
@@ -1,7 +1,7 @@
 export const initStatistics: State.Statistics = {
   tipBlockNumber: '0',
   currentEpochAverageBlockTime: '0',
-  currentEpochDifficulty: 0,
+  currentEpochDifficulty: '0',
   hashRate: '0',
 }
 
