dc504c554ef46d74c328efee3d4b86e81689ceaa
duanyytop <duanyytop@gmail.com>
2019-10-18 17:00:39 +0800

    feat: impl dao tab bar and dao search

--- src/pages/NervosDao/index.tsx
+++ src/pages/NervosDao/index.tsx
@@ -1,16 +1,22 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { StateWithDispatch } from '../../contexts/providers/reducer'
 import { AppContext } from '../../contexts/providers'
 import Content from '../../components/Content'
 import HashCard from '../../components/Card/HashCard'
 import i18n from '../../utils/i18n'
-import { DaoContentPanel } from './styled'
+import { DaoContentPanel, DaoTabBarPanel } from './styled'
 import OverviewCard, { OverviewItemData } from '../../components/Card/OverviewCard'
 import { localeNumberString } from '../../utils/number'
 import { isMobile } from '../../utils/screen'
 import { getNervosDao, getNervosDaoTransactions } from '../../service/app/nervosDao'
 import { shannonToCkb } from '../../utils/util'
 import DaoTransactions from './DaoTransactions'
+import DaoSearch from '../../components/Search/DaoSearch'
+
+enum DaoTab {
+  Transactions,
+  Depositor,
+}
 
 const DervosDaoOverview = ({ nervosDao }: { nervosDao: State.NervosDao }) => {
   const overviewItems: OverviewItemData[] = [
@@ -43,6 +49,7 @@ const DervosDaoOverview = ({ nervosDao }: { nervosDao: State.NervosDao }) => {
 
 export const NervosDao = ({ dispatch }: React.PropsWithoutRef<StateWithDispatch>) => {
   const { nervosDaoState } = useContext(AppContext)
+  const [daoTab, setDaoTab] = useState(DaoTab.Transactions)
 
   useEffect(() => {
     getNervosDao(dispatch)
@@ -58,7 +65,32 @@ export const NervosDao = ({ dispatch }: React.PropsWithoutRef<StateWithDispatch>
           dispatch={dispatch}
         />
         <DervosDaoOverview nervosDao={nervosDaoState.nervosDao} />
-        <DaoTransactions />
+
+        <DaoTabBarPanel>
+          <div className="nervos_dao_tab_bar">
+            <div
+              role="button"
+              tabIndex={-1}
+              onKeyDown={() => {}}
+              className={daoTab === DaoTab.Transactions ? 'tab_bar_selected' : 'tab_bar_normal'}
+              onClick={() => setDaoTab(DaoTab.Transactions)}
+            >
+              Transactions
+            </div>
+            <div
+              role="button"
+              tabIndex={-1}
+              onKeyDown={() => {}}
+              className={daoTab === DaoTab.Depositor ? 'tab_bar_selected' : 'tab_bar_normal'}
+              onClick={() => setDaoTab(DaoTab.Depositor)}
+            >
+              Depositor
+            </div>
+          </div>
+          {daoTab === DaoTab.Transactions && <DaoSearch dispatch={dispatch} />}
+        </DaoTabBarPanel>
+
+        {daoTab === DaoTab.Transactions ? <DaoTransactions /> : <div>Rank</div>}
       </DaoContentPanel>
     </Content>
   )
--- src/pages/NervosDao/styled.tsx
+++ src/pages/NervosDao/styled.tsx
@@ -14,6 +14,54 @@ export const DaoContentPanel = styled.div`
   }
 `
 
+export const DaoTabBarPanel = styled.div`
+  width: 100%;
+  height: 70px;
+  background: white;
+  display: flex;
+  margin: 20px 0 5px 0;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 40px;
+
+  @media (max-width: 700px) {
+    height: 90px;
+    flex-direction: column;
+    justify-content: center;
+  }
+
+  .nervos_dao_tab_bar {
+    display: flex;
+    height: 30px;
+    cursor: pointer;
+
+    @media (max-width: 700px) {
+      margin-bottom: 23px;
+    }
+
+    .div {
+      font-size: 20px;
+
+      @media (max-width: 700px) {
+        font-size: 13px;
+      }
+    }
+
+    div: nth-child(2) {
+      margin-left: 50px;
+    }
+
+    .tab_bar_normal {
+      color: #000000;
+    }
+
+    .tab_bar_selected {
+      color: ${props => props.theme.primary};
+      border-bottom: 1px solid ${props => props.theme.primary};
+    }
+  }
+`
+
 export const TransactionsPagition = styled.div`
   margin: 20px 0px 0px 0px;
   width: 100%;
