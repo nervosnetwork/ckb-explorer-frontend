2294457b012baa91ffe643215a6f8d2a86466e55
duanyytop <duanyytop@gmail.com>
2019-10-23 10:26:04 +0800

    chore: remove useless annotation for blockchain info

--- src/contexts/providers/hook.ts
+++ src/contexts/providers/hook.ts
@@ -1,6 +1,6 @@
 import { useEffect, useState, useContext } from 'react'
 import { initAxiosInterceptors } from '../../service/http/interceptors'
-import { getChainType } from '../../service/app/blockchain'
+import { getChainType, handleBlockchainAlert } from '../../service/app/blockchain'
 import { BLOCKCHAIN_ALERT_POLLING_TIME, RESIZE_LATENCY, CachedKeys } from '../../utils/const'
 import { initNodeVersion } from '../../service/app/nodeInfo'
 import { AppDispatch, AppActions } from './reducer'
@@ -58,7 +58,7 @@ export const useInitApp = (dispatch: AppDispatch) => {
   useWindowResize(dispatch)
 
   useInterval(() => {
-    // handleBlockchainAlert(dispatch)
+    handleBlockchainAlert(dispatch)
   }, BLOCKCHAIN_ALERT_POLLING_TIME)
 }
 
