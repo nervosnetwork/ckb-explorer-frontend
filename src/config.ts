082e6334713ba2b2b514526991ae74ba6312a720
duanyytop <duanyytop@gmail.com>
2019-10-29 11:58:20 +0800

    feat: add basename for router

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,5 @@
 export default {
   API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
   CHAIN_TYPE: process.env.REACT_APP_CHAIN_TYPE || 'mainnet',
+  CHAIN_NAME: process.env.REACT_APP_CHAIN_NAME || '',
 }
