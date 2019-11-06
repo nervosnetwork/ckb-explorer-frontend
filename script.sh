2bc85839f942fc046bb82a4fc17e69393e8088b3
duanyytop <duanyytop@gmail.com>
2019-11-06 17:05:30 +0800

    feat: add shell script for tesnet deploying

--- script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-js1="/testnet/`find build/static/js/2.*.js`"
-js2="/testnet/`find build/static/js/main.*.js`"
-sed  -e "s#/build/static/js/main.[^\"]*.js#${js2}#g" -e "s#/build/static/js/2.[^\"]*.js#${js1}#g"  -i build/index.html
