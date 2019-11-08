a60f9b41f629546705af0204404c2c4ce037053a
duanyytop <duanyytop@gmail.com>
2019-11-08 16:10:56 +0800

    chore: update deploy test shell script

--- deploy_testnet.sh
+++ deploy_testnet.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 js1="/testnet/`find static/js/2.*.js`"
 js2="/testnet/`find static/js/main.*.js`"
-sed  -e "s#/static/js/main.[^\"]*.js#${js2}#g" -e "s#/static/js/2.[^\"]*.js#${js1}#g"  -i '' index.html
+sed  -e "s#/static/js/main.[^\"]*.js#${js2}#g" -e "s#/static/js/2.[^\"]*.js#${js1}#g"  -i index.html
