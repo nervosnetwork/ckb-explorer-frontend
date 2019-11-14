a808b03b1a050c1a317b41a4cb9cb19ae5f645a3
duanyytop <duanyytop@gmail.com>
2019-11-14 21:17:20 +0800

    chore: hide chain switching component

--- src/components/Header/styled.tsx
+++ src/components/Header/styled.tsx
@@ -168,7 +168,7 @@ export const HeaderBlockchainPanel = styled.div`
     text-align: center;
     letter-spacing: 2px;
     font-weight: bold;
-    cursor: pointer;
+    // cursor: pointer;
   }
 
   @media (max-width: 700px) {
@@ -191,7 +191,7 @@ export const HeaderVersionPanel = styled.div`
   font-size: 12px;
   margin-right: 90px;
   display: flex;
-  cursor: pointer;
+  // cursor: pointer;
 
   > div {
     margin-top: 8px;
