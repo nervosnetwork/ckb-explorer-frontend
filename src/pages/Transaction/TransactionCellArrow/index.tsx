9214eb5230103576da0ca9f470406c4d44ba15ff
duanyytop <duanyytop@gmail.com>
2019-11-30 08:40:42 +0800

    chore: update live cell icon and position

--- src/pages/Transaction/TransactionCellArrow/index.tsx
+++ src/pages/Transaction/TransactionCellArrow/index.tsx
@@ -2,48 +2,41 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { CellType } from '../../../utils/const'
-import RightHighlightArrow from '../../../assets/right_green_arrow.png'
-import RightHighlightBlueArrow from '../../../assets/right_blue_arrow.png'
-import RightNormalArrow from '../../../assets/right_grey_arrow.png'
+import RightGreenArrow from '../../../assets/right_green_arrow.png'
+import RightBlueArrow from '../../../assets/right_blue_arrow.png'
+import LiveCellIcon from '../../../assets/live_cell.png'
 import { isMainnet } from '../../../utils/chain'
 
 const LeftArrowImage = styled.img`
   width: 16px;
-  height: 12px;
+  height: auto;
   margin: 0px 7px 0 0;
 
   @media (max-width: 700px) {
-    margin: ${({ marginTop }: { marginTop: boolean }) => (marginTop ? '0px 10px 0 0' : '0px 10px 0 0')};
+    width: 12px;
+    margin: 0 10px 0 0;
   }
 `
 
 const RightArrowImage = styled.img`
   width: 16px;
-  height: 12px;
-  margin: 0px 0 0 7px;
+  height: auto;
+  margin: 0px 0 3px 7px;
 
   @media (max-width: 700px) {
-    margin: ${({ marginTop }: { marginTop: boolean }) => (marginTop ? '0px 5px 0 7px' : '0px 5px 0 7px')};
+    width: 12px;
+    margin: 0 5px 0 7px;
   }
 `
 
-export default ({
-  cell,
-  cellType,
-  haveMarginTop,
-}: {
-  cell: State.Cell
-  cellType: CellType
-  haveMarginTop?: boolean
-}) => {
+export default ({ cell, cellType }: { cell: State.Cell; cellType: CellType }) => {
   if (cellType === CellType.Input) {
     return cell.generatedTxHash ? (
       <Link to={`/transaction/${cell.generatedTxHash}`}>
         <LeftArrowImage
           className="transaction__cell_left_arrow"
-          src={isMainnet() ? RightHighlightArrow : RightHighlightBlueArrow}
+          src={isMainnet() ? RightGreenArrow : RightBlueArrow}
           alt="left arrow"
-          marginTop={!!haveMarginTop}
         />
       </Link>
     ) : null
@@ -52,17 +45,11 @@ export default ({
     <Link to={`/transaction/${cell.consumedTxHash}`}>
       <RightArrowImage
         className="transaction__cell_right_arrow"
-        src={isMainnet() ? RightHighlightArrow : RightHighlightBlueArrow}
+        src={isMainnet() ? RightGreenArrow : RightBlueArrow}
         alt="right arrow"
-        marginTop={!!haveMarginTop}
       />
     </Link>
   ) : (
-    <RightArrowImage
-      className="transaction__cell_right_arrow"
-      src={RightNormalArrow}
-      alt="right arrow"
-      marginTop={!!haveMarginTop}
-    />
+    <RightArrowImage className="transaction__cell_right_arrow" src={LiveCellIcon} alt="right arrow" />
   )
 }
