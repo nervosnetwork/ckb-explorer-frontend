33c5662b48bb141e1191e64d33c7fb1702afc87f
duanyytop <duanyytop@gmail.com>
2019-12-11 09:36:05 +0800

    chore: update footer background color

--- src/components/Footer/styled.tsx
+++ src/components/Footer/styled.tsx
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 export const FooterDiv = styled.div`
   width: 100%;
   overflow: hidden;
-  background-color: #424242;
+  background-color: #040607;
   display: flex;
   flex-direction: column;
 
