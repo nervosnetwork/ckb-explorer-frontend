0068bc855b405fc29d6f50e722a7ae77847c27f2
duanyytop <duanyytop@gmail.com>
2020-02-04 15:28:08 +0800

    chore: update self cell background color

--- src/components/TransactionItem/TransactionItemCell/styled.tsx
+++ src/components/TransactionItem/TransactionItemCell/styled.tsx
@@ -6,7 +6,7 @@ export const TransactionCellPanel = styled.div`
   justify-content: space-between;
   margin-top: 20px;
 
-  background: ${({ highLight = false }: { highLight?: boolean }) => (highLight ? '' : '#e3e3e3')};
+  background: ${({ highLight = false }: { highLight?: boolean }) => (highLight ? '' : '#f5f5f5')};
 
   @media (min-width: 700px) {
     height: 20px;
