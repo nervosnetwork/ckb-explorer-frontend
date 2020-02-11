dc86522c791ed61aa1e1d3ce1ae456f0d6e34d1f
duanyytop <duanyytop@gmail.com>
2020-02-11 16:26:09 +0800

    chore: update page content margin top

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -1,27 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const parseMargin = (width: number) => {
-  if (width >= 1200) {
-    return 64
-  }
-  return 64 / (width / 1200)
-}
-
 const ContentDiv = styled.div`
   width: 100%;
   overflow-x: hidden;
   flex: 1;
-  margin-top: ${(props: { width: number }) => `${parseMargin(props.width)}px`};
+  margin-top: 64px;
 
   @media (max-width: 700px) {
     margin-top: 42px;
   }
 `
 export default ({ children, style }: { children: any; style?: any }) => {
-  return (
-    <ContentDiv style={style} width={window.innerWidth}>
-      {children}
-    </ContentDiv>
-  )
+  return <ContentDiv style={style}>{children}</ContentDiv>
 }
