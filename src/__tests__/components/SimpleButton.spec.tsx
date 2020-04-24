014786b38bb863579133ca705314fe38c9863866
duanyytop <duanyytop@gmail.com>
2020-04-24 14:34:03 +0800

    test: add test cases to Sheet and SimpleButton

---
+++ src/__tests__/components/SimpleButton.spec.tsx
@@ -0,0 +1,29 @@
+import React, { ReactElement } from 'react'
+import { shallow } from 'enzyme'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import SimpleButton from '../../components/SimpleButton'
+
+describe('Search Component', () => {
+  let component: ReactElement
+
+  beforeAll(() => {
+    component = (
+      <SimpleButton>
+        <div className="button__child">children</div>
+      </SimpleButton>
+    )
+  })
+
+  it('shallow renders', () => {
+    const wrapper = renderer.create(component).toJSON()
+
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  it('Component Render', () => {
+    const wrapper = shallow(component)
+    expect(wrapper).toBeDefined()
+    expect(wrapper.find('.button__child').text()).toBe('children')
+  })
+})
