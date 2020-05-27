dffa426373c1328647efec8a82f839ed7190400b
duanyytop <duanyytop@gmail.com>
2020-05-27 18:16:20 +0800

    chore: update contract tag component

--- src/components/HashTag/styled.tsx
+++ src/components/HashTag/styled.tsx
@@ -3,6 +3,7 @@ import styled from 'styled-components'
 export const TagPanel = styled.div`
   height: 20px;
   width: ${({ length }: { length: number; isLock?: boolean }) => `${length * 7.5}px`};
+  min-width: ${({ length }: { length: number }) => `${length * 7.5}px`};
   border-radius: 4px;
   border: solid 0.5px ${({ isLock }: { isLock?: boolean }) => (isLock ? '#b1caff' : '#caacef')};
   background-color: ${({ isLock }: { isLock?: boolean }) => (isLock ? '#d8e4ff' : '#f0e0fb')};
