bba25b5cc617950fda74d7096422666dc8410725
duanyytop <duanyytop@gmail.com>
2020-06-30 13:41:22 +0800

    Fix tx list table algin

--- src/components/Table/styled.tsx
+++ src/components/Table/styled.tsx
@@ -75,8 +75,7 @@ export const TableContentRowItem = styled.div`
 
 export const TableMinerContentPanel = styled.div`
   width: ${(props: { width: string }) => props.width};
-  line-height: 30px;
-  height: 30px;
+  margin: auto 0;
   text-align: center;
   margin: auto 0;
   .table__miner__content {
