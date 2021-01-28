d21329402c890a4a441201e5ff041b3ea74d20fc
duanyytop <duanyytop@gmail.com>
2021-01-28 13:39:26 +0800

    feat: Add cheque script tag for Aggron

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -223,4 +223,10 @@ export const TestnetContractHashTags: ContractHashTag[] = [
     tag: 'pwlock-r1',
     category: 'lock',
   },
+  {
+    codeHashes: ['0x9f27f3afc8d26dfa8bc0c8fa21bc033ddcdab6ad83d5e865cdd6d5d0b3b95642'],
+    txHashes: ['0x1dbbeac82db9a330ed07dd33e547facbca14378196f0e2d69ad8e83bce1d5f54-0'],
+    tag: 'cheque',
+    category: 'lock',
+  },
 ]
