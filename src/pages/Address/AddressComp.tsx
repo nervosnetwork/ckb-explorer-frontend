e8426ba75a2b11393088b6c52c5b1c2f5883d152
Dylan <duanyytop@gmail.com>
2021-05-31 20:38:21 +0800

    chore: Slice address nft name

--- src/pages/Address/AddressComp.tsx
+++ src/pages/Address/AddressComp.tsx
@@ -18,6 +18,7 @@ import TitleCard from '../../components/Card/TitleCard'
 import CKBTokenIcon from '../../assets/ckb_token_icon.png'
 import SUDTTokenIcon from '../../assets/sudt_token.png'
 import { isMobile } from '../../utils/screen'
+import { sliceNftName } from '../../utils/string'
 
 const addressAssetInfo = (address: State.Address) => {
   const items = [
@@ -57,7 +58,7 @@ const AddressUDTItem = ({ udtAccount }: { udtAccount: State.UDTAccount }) => {
     <AddressUDTItemPanel href={`${baseUrl()}/sudt/${typeHash}`} isLink={isSudt}>
       <img className="address__udt__item__icon" src={udtIconFile || SUDTTokenIcon} alt="udt icon" />
       <div className="address__udt__item__info">
-        <span>{symbol}</span>
+        <span>{isSudt ? symbol : sliceNftName(symbol)}</span>
         <span>{isSudt ? parseUDTAmount(amount, decimal) : amount}</span>
       </div>
     </AddressUDTItemPanel>
--- src/utils/string.ts
+++ src/utils/string.ts
@@ -149,3 +149,5 @@ export const handleBigNumberFloor = (value: BigNumber | string | number, decimal
   }
   return `${parseFloorDecimal(bigValue, decimal)}`
 }
+
+export const sliceNftName = (name: string) => (name.length > 20 ? `${name.slice(0, 20)}...` : name)
