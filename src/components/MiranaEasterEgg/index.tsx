91d4e60b7d14a6e80fe69eff9b493d155369392f
yanguoyu <841185308@qq.com>
2022-05-10 16:26:16 +0800

    fix: Testnet not show text.

--- src/components/MiranaEasterEgg/index.tsx
+++ src/components/MiranaEasterEgg/index.tsx
@@ -1,3 +1,4 @@
+import { isMainnet } from '../../utils/chain'
 import styles from './index.module.scss'
 
 const miranaAliveText = `Mirana is live`
@@ -5,13 +6,15 @@ const miranaAliveText = `Mirana is live`
 export default () => {
   return (
     <div className={styles.Root} data-alive>
-      <div className={styles.TextContain}>
-        <h1 className={styles.Text} data-text={miranaAliveText}>
-          {miranaAliveText}
-        </h1>
-        <div className={styles.Gradient} />
-        <div className={styles.Spotlight} />
-      </div>
+      {isMainnet() && (
+        <div className={styles.TextContain}>
+          <h1 className={styles.Text} data-text={miranaAliveText}>
+            {miranaAliveText}
+          </h1>
+          <div className={styles.Gradient} />
+          <div className={styles.Spotlight} />
+        </div>
+      )}
     </div>
   )
 }
