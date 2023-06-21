e37fec111e1dc008a2b8868d8a973ae0394b71f0
zhb666 <462612421@qq.com>
2023-06-21 17:50:11 +0800

    feat: The Cell Count chart data is abnormal (#11)
    Co-authored-by: Chen Yu <keithwhisper@gmail.com>

--- src/pages/StatisticsChart/activities/CellCount.tsx
+++ src/pages/StatisticsChart/activities/CellCount.tsx
@@ -7,14 +7,17 @@ import { tooltipColor, tooltipWidth, SeriesItem, SmartChartPage } from '../commo
 import { ChartCachedKeys } from '../../../constants/cache'
 import { fetchStatisticCellCount } from '../../../service/http/fetcher'
 
-const widthSpan = (value: string) => tooltipWidth(value, currentLanguage() === 'en' ? 60 : 80)
+const widthSpan = (value: string) => tooltipWidth(value, currentLanguage() === 'en' ? 125 : 80)
 
-const parseTooltip = ({ seriesName, data, color }: SeriesItem & { data: [string, string, string] }): string => {
-  if (seriesName === i18n.t('statistic.live_cell')) {
-    return `<div>${tooltipColor(color)}${widthSpan(i18n.t('statistic.live_cell'))} ${handleAxis(data[1], 2)}</div>`
+const parseTooltip = ({ seriesName, data, color }: SeriesItem & { data: [string, string, string, string] }): string => {
+  if (seriesName === i18n.t('statistic.dead_cell')) {
+    return `<div>${tooltipColor(color)}${widthSpan(i18n.t('statistic.dead_cell'))} ${handleAxis(data[2], 2)}</div>`
   }
   if (seriesName === i18n.t('statistic.all_cells')) {
-    return `<div>${tooltipColor(color)}${widthSpan(i18n.t('statistic.all_cells'))} ${handleAxis(data[2], 2)}</div>`
+    return `<div>${tooltipColor(color)}${widthSpan(i18n.t('statistic.all_cells'))} ${handleAxis(data[1], 2)}</div>`
+  }
+  if (seriesName === i18n.t('statistic.live_cell')) {
+    return `<div>${tooltipColor(color)}${widthSpan(i18n.t('statistic.live_cell'))} ${handleAxis(data[3], 2)}</div>`
   }
   return ''
 }
@@ -27,7 +30,7 @@ const getOption = (
 ): echarts.EChartOption => {
   const gridThumbnail = {
     left: '4%',
-    right: '4%',
+    right: '10%',
     top: '8%',
     bottom: '6%',
     containLabel: true,
@@ -45,7 +48,7 @@ const getOption = (
       ? {
           trigger: 'axis',
           formatter: (dataList: any) => {
-            const list = dataList as Array<SeriesItem & { data: [string, string, string] }>
+            const list = dataList as Array<SeriesItem & { data: [string, string, string, string] }>
             let result = `<div>${tooltipColor('#333333')}${widthSpan(i18n.t('statistic.date'))} ${
               list[0].data[0]
             }</div>`
@@ -56,18 +59,21 @@ const getOption = (
           },
         }
       : undefined,
-    legend: !isThumbnail
-      ? {
-          data: [
+    legend: {
+      data: isThumbnail
+        ? []
+        : [
+            {
+              name: i18n.t('statistic.all_cells'),
+            },
             {
               name: i18n.t('statistic.live_cell'),
             },
             {
-              name: i18n.t('statistic.all_cells'),
+              name: i18n.t('statistic.dead_cell'),
             },
           ],
-        }
-      : undefined,
+    },
     grid: isThumbnail ? gridThumbnail : grid,
     dataZoom: isThumbnail ? [] : DATA_ZOOM_CONFIG,
     xAxis: [
@@ -82,7 +88,6 @@ const getOption = (
     yAxis: [
       {
         position: 'left',
-        name: isMobile || isThumbnail ? '' : i18n.t('statistic.live_cell'),
         type: 'value',
         scale: true,
         axisLine: {
@@ -94,59 +99,64 @@ const getOption = (
           formatter: (value: string) => handleAxis(new BigNumber(value)),
         },
       },
-      {
-        position: 'right',
-        name: isMobile || isThumbnail ? '' : i18n.t('statistic.all_cells'),
-        type: 'value',
-        scale: true,
-        axisLine: {
-          lineStyle: {
-            color: chartColor.colors[1],
-          },
-        },
-        axisLabel: {
-          formatter: (value: string) => handleAxis(new BigNumber(value)),
-        },
-      },
     ],
     series: [
+      {
+        name: i18n.t('statistic.all_cells'),
+        type: 'line',
+        yAxisIndex: 0,
+        symbol: isThumbnail ? 'none' : 'circle',
+        symbolSize: 3,
+        areaStyle: {
+          color: chartColor.colors[0],
+        },
+        lineStyle: {
+          width: 4,
+        },
+      },
+      {
+        name: i18n.t('statistic.dead_cell'),
+        type: 'line',
+        stack: 'sum',
+        yAxisIndex: 0,
+        symbol: isThumbnail ? 'none' : 'circle',
+        symbolSize: 3,
+        areaStyle: {
+          color: chartColor.colors[1],
+        },
+      },
       {
         name: i18n.t('statistic.live_cell'),
         type: 'line',
         yAxisIndex: 0,
         symbol: isThumbnail ? 'none' : 'circle',
+        stack: 'sum',
         symbolSize: 3,
-        encode: {
-          x: 'timestamp',
-          y: 'live',
-        },
-      },
-      {
-        name: i18n.t('statistic.all_cells'),
-        type: 'line',
-        yAxisIndex: 1,
-        symbol: isThumbnail ? 'none' : 'circle',
-        symbolSize: 3,
-        encode: {
-          x: 'timestamp',
-          y: 'all',
+        areaStyle: {
+          color: chartColor.colors[2],
         },
       },
     ],
     dataset: {
       source: statisticCellCounts.map(data => [
         parseDateNoTime(data.createdAtUnixtimestamp),
-        new BigNumber(data.liveCellsCount).toNumber(),
-        new BigNumber(data.allCellsCount).toNumber(),
+        new BigNumber(data.allCellsCount).toFixed(0),
+        new BigNumber(data.deadCellsCount).toFixed(0),
+        new BigNumber(data.liveCellsCount).toFixed(0),
       ]),
-      dimensions: ['timestamp', 'live', 'all'],
+      dimensions: ['timestamp', 'all', 'live', 'dead'],
     },
   }
 }
 
 const toCSV = (statisticCellCounts: State.StatisticCellCount[]) =>
   statisticCellCounts
-    ? statisticCellCounts.map(data => [data.createdAtUnixtimestamp, data.liveCellsCount, data.allCellsCount])
+    ? statisticCellCounts.map(data => [
+        data.createdAtUnixtimestamp,
+        data.allCellsCount,
+        data.liveCellsCount,
+        data.deadCellsCount,
+      ])
     : []
 
 export const CellCountChart = ({ isThumbnail = false }: { isThumbnail?: boolean }) => {
