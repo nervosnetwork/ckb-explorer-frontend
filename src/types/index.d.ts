466fb752a68ee667f9b47354a09839c2864e7a65
WhiteMind <WhiteMind@qq.com>
2023-09-27 18:29:24 +0800

    refactor: optimize and simplify the routes code

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -601,16 +601,6 @@ declare namespace State {
   type SortOrderTypes = 'asc' | 'desc'
 }
 
-declare namespace CustomRouter {
-  interface Route {
-    name: string
-    path: string
-    params?: string
-    exact?: boolean
-    comp: React.FunctionComponent<any>
-  }
-}
-
 declare namespace Response {
   export interface Response<T> {
     data: T
