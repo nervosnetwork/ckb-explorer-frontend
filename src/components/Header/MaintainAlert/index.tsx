e633604822ef00192394ec535091091235f26906
Keith <keithwhisper@gmail.com>
2024-01-17 22:55:59 +0900

    fix: optimize maintenance message

--- src/components/Header/MaintainAlert/index.tsx
+++ src/components/Header/MaintainAlert/index.tsx
@@ -1,14 +1,52 @@
+import { useQuery } from '@tanstack/react-query'
 import { useTranslation } from 'react-i18next'
-import { IS_MAINTAINING } from '../../../constants/common'
+import axios from 'axios'
+import config from '../../../config'
+import { useLatestBlockNumber } from '../../../services/ExplorerService'
 import styles from './styles.module.scss'
+
+const { BACKUP_NODES: backupNodes } = config
+const thredhold = 20
+
+const getTipFromNode = (url: string) =>
+  axios<any, { result: string }>(url, {
+    method: 'post',
+    data: {
+      jsonrpc: '2.0',
+      id: 1,
+      method: 'get_tip_block_number',
+      params: [],
+    },
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    // FIXME: type declaration
+  }).then((res: any) => res.data.result)
 
 const MaintainAlert = () => {
   const { t } = useTranslation()
-  if (IS_MAINTAINING) {
-    return <div className={styles.container}>{t('error.maintain')}</div>
-  }
+  const synced = useLatestBlockNumber()
+  const { data: tip } = useQuery(
+    ['backup_nodes'],
+    async () => {
+      try {
+        if (backupNodes.length === 0) return null
 
-  return null
+        // FIXME: type declaration
+        const [tip1, tip2]: any = await Promise.allSettled(backupNodes.map(getTipFromNode))
+        if (!tip1.value && !tip2.value) return null
+        if (+tip1.value > +tip2.value) return +tip1.value
+        return +tip2.value
+      } catch {
+        return null
+      }
+    },
+    { refetchInterval: 12 * 1000 },
+  )
+
+  const isMaintained = tip && synced && tip - synced > thredhold
+
+  return isMaintained ? <div className={styles.container}>{t('error.maintain', { tip, lag: tip - synced })}</div> : null
 }
 
 export default MaintainAlert
--- src/config.ts
+++ src/config.ts
@@ -2,4 +2,5 @@ export default {
   API_URL: process.env.REACT_APP_API_URL || 'http://localhost:3000',
   CHAIN_TYPE: process.env.REACT_APP_CHAIN_TYPE || 'mainnet',
   BASE_URL: process.env.REACT_BASE_URL || 'explorer.nervos.org/',
+  BACKUP_NODES: process.env.REACT_APP_BACKUP_NODES?.split(',') || [],
 }
