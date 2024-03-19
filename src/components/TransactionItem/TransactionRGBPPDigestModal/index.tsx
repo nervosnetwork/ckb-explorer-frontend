a0fbc8a44fd74f37e2f76f9a71b3d5a209fc949e
daryl <sai9375@hotmail.com>
2024-03-19 17:06:22 +0800

    style(rgb++): use exist close icon

--- src/components/TransactionItem/TransactionRGBPPDigestModal/index.tsx
+++ src/components/TransactionItem/TransactionRGBPPDigestModal/index.tsx
@@ -1,7 +1,7 @@
 import { useTranslation } from 'react-i18next'
 import { useQuery } from '@tanstack/react-query'
 import styles from './styles.module.scss'
-import { ReactComponent as CloseIcon } from '../../../assets/close_icon.svg'
+import CloseIcon from '../../../assets/modal_close.png'
 import { explorerService } from '../../../services/ExplorerService'
 import { TransactionRGBPPDigestContent } from './TransactionRGBPPDigestContent'
 import { TransactionLeapDirection } from './types'
@@ -23,7 +23,9 @@ const TransactionRGBPPDigestModal = ({
     <div className={styles.container}>
       <div className={styles.header}>
         <span className={styles.left}>{t('address.transaction_rgbpp_digest')}</span>
-        <CloseIcon className={styles.right} onClick={() => onClickClose()} />
+        <button onClick={() => onClickClose()} type="button">
+          <img src={CloseIcon} alt="close icon" />
+        </button>
       </div>
       {data && <TransactionRGBPPDigestContent tx={data.data} leapDirection={leapDirection} />}
     </div>
