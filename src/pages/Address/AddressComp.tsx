1637a83bd3f60246d1ec46ab64e34a2e66c31a61
Keith <keithwhisper@gmail.com>
2024-03-20 17:56:36 +0900

    Revert "fix(cell): fix the live cell style"
    This reverts commit 1de1558bbaeb48be173c9ee4c86d87bcebd4234a.

--- src/pages/Address/AddressComp.tsx
+++ src/pages/Address/AddressComp.tsx
@@ -404,9 +404,9 @@ export const AddressOverviewCard: FC<{ address: Address }> = ({ address }) => {
                 }
                 key={AssetInfo.CELLs}
               >
-                <AddressUDTAssetsList>
+                <div className="addressUdtAssetsGrid">
                   <Cells address={address.addressHash} count={+address.liveCellsCount} />
-                </AddressUDTAssetsList>
+                </div>
               </AddressAssetsTabPane>
             ) : null}
           </AddressAssetsTab>
