81d2bf44887675a1b8985a2ebc1386110abaf396
Keith <keithwhisper@gmail.com>
2024-04-04 00:26:43 +0900

    feat: parse xudt in live cells

--- src/pages/Address/Cells.tsx
+++ src/pages/Address/Cells.tsx
@@ -121,6 +121,15 @@ const Cell: FC<{ cell: LiveCell }> = ({ cell }) => {
       detailInfo = cell.extraInfo.amount
       break
     }
+    case 'xudt': {
+      icon = SUDTTokenIcon
+      assetName = cell.extraInfo.symbol || 'xUDT'
+      attribute = cell.extraInfo.decimal
+        ? parseUDTAmount(cell.extraInfo.amount, cell.extraInfo.decimal)
+        : 'Unknown UDT amount'
+      detailInfo = cell.extraInfo.amount
+      break
+    }
     case 'omiga_inscription': {
       icon = SUDTTokenIcon
       assetName = cell.extraInfo.symbol || t('udt.inscription')
