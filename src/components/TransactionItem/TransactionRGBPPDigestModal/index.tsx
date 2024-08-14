4bc1e6d5550d08ab326b67964eb65853ba859942
daryl <sai9375@hotmail.com>
2024-08-14 17:10:32 +0800

    fix(rgb++): loading picture when loading (#411)
    * fix(rgb++): loading picture when loading
    
    * feat(loading): new pic
    
    * fix(rgbdigest): fixed position
    
    * fix(rgbdigest): loading

--- src/components/TransactionItem/TransactionRGBPPDigestModal/index.tsx
+++ src/components/TransactionItem/TransactionRGBPPDigestModal/index.tsx
@@ -6,11 +6,12 @@ import CloseIcon from '../../../assets/modal_close.png'
 import { TransactionRGBPPDigestContent } from './TransactionRGBPPDigestContent'
 import { explorerService } from '../../../services/ExplorerService'
 import { TransactionLeapDirection } from '../../RGBPP/types'
+import SmallSpinner from '../../AwesomeLoadings/SmallSpinner'
 
 const TransactionRGBPPDigestModal = ({ hash, onClickClose }: { onClickClose: Function; hash: string }) => {
   const { t } = useTranslation()
 
-  const { data, isFetched } = useQuery(['rgb-digest', hash], () => explorerService.api.fetchRGBDigest(hash))
+  const { data, isFetched, isLoading } = useQuery(['rgb-digest', hash], () => explorerService.api.fetchRGBDigest(hash))
 
   const direction = useMemo(() => {
     switch (data?.data.leapDirection) {
@@ -25,25 +26,30 @@ const TransactionRGBPPDigestModal = ({ hash, onClickClose }: { onClickClose: Fun
     }
   }, [data])
 
-  if (isFetched && data?.data) {
-    return (
-      <div className={styles.container}>
-        <div className={styles.header}>
-          <div className={styles.left}>
-            <span>{t('address.transaction_rgbpp_digest')}</span>
-            {direction !== TransactionLeapDirection.NONE && (
-              <span className={styles.leap}>{t(`address.leap_${direction}`)}</span>
-            )}
-          </div>
-          <button onClick={() => onClickClose()} type="button" className={styles.buttonClose}>
-            <img src={CloseIcon} alt="close icon" className={styles.closeIcon} />
-          </button>
+  return (
+    <div className={styles.container}>
+      {isLoading ? (
+        <div style={{ margin: 'auto', width: '100%', display: 'flex' }}>
+          <SmallSpinner />
         </div>
-        <TransactionRGBPPDigestContent hash={hash} digest={data?.data} isFetched={isFetched} />
-      </div>
-    )
-  }
-  return null
+      ) : (
+        <>
+          <div className={styles.header}>
+            <div className={styles.left}>
+              <span>{t('address.transaction_rgbpp_digest')}</span>
+              {direction !== TransactionLeapDirection.NONE && (
+                <span className={styles.leap}>{t(`address.leap_${direction}`)}</span>
+              )}
+            </div>
+            <button onClick={() => onClickClose()} type="button" className={styles.buttonClose}>
+              <img src={CloseIcon} alt="close icon" className={styles.closeIcon} />
+            </button>
+          </div>
+          <TransactionRGBPPDigestContent hash={hash} digest={data?.data} isFetched={isFetched} />
+        </>
+      )}
+    </div>
+  )
 }
 
 export default TransactionRGBPPDigestModal
