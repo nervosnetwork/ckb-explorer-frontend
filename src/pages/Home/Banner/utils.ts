c20b5e4451e7a1e9cea3855ca4e1cf433e79ba22
Keith <keithwhisper@gmail.com>
2024-11-29 15:03:39 +0900

    fix: fix algorithm of knowledge size

--- src/pages/Home/Banner/utils.ts
+++ src/pages/Home/Banner/utils.ts
@@ -1,3 +1,4 @@
+import { ckbDecimals } from '@ckb-lumos/bi'
 import BigNumber from 'bignumber.js'
 
 /**
@@ -5,6 +6,7 @@ import BigNumber from 'bignumber.js'
  * ref: https://talk.nervos.org/t/how-to-get-the-average-occupied-bytes-per-live-cell-in-ckb/7138/2?u=keith
  * */
 const EXCLUDE = BigNumber('504000000000000000')
+const CKB_DECIMAL = BigNumber(10).pow(ckbDecimals)
 
 export const getKnowledgeSize = async (nodeUrl: string) => {
   const header = await fetch(nodeUrl, {
@@ -25,6 +27,6 @@ export const getKnowledgeSize = async (nodeUrl: string) => {
     .slice(2)
     .match(/\w{16}/g)
     .map((i: string) => i.match(/\w{2}/g)?.reverse().join('') ?? '')
-  const total = BigNumber(`0x${u}`).minus(EXCLUDE).toFormat()
+  const total = BigNumber(`0x${u}`).minus(EXCLUDE).div(CKB_DECIMAL).toNumber()
   return total
 }
