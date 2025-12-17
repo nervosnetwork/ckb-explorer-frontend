89f2814f2112a9a0c03a040bc255cc02741e5665
panghu <964001566@qq.com>
2025-12-17 16:21:53 +0800

    update css

--- src/components/Notification/index.tsx
+++ src/components/Notification/index.tsx
@@ -4,16 +4,13 @@ import { ReactComponent as InfoICon } from './info.svg'
 
 const Notification = () => {
   const { t } = useTranslation()
-
   const url = 'https://discord.gg/TcFdtES68c'
-  //   const translatedText = t('notes.title', { url })
   return (
     <div className={styles.container}>
-      <InfoICon />
-      {/* <div dangerouslySetInnerHTML={{ __html: translatedText }} /> */}
-      <div>
+      <InfoICon className={styles.icon} />
+      <div className={styles.content}>
         {t('notes.titlepre', { url })}
-        <a href={url} target="_blank" rel="noreferrer">
+        <a href={url} target="_blank" rel="noreferrer" className={styles.link}>
           {url}
         </a>
         {t('notes.titlenext')}
--- src/components/Page/index.tsx
+++ src/components/Page/index.tsx
@@ -1,10 +1,8 @@
 import { CSSProperties, ReactNode } from 'react'
 import styles from './index.module.scss'
-import Notification from '../Notification'
 
 export default ({ children, style }: { children: ReactNode; style?: CSSProperties }) => (
   <div className={styles.pagePanel} style={style}>
-    <Notification />
     {children}
   </div>
 )
